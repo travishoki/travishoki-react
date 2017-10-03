3eabfd4c690be7cf53561cc1dab8e2ca5fe2bdf0
Travis Hoki <travis.hoki@gmail.com>
2017-10-02 22:11:53 -0600

    update family page

--- src/components/about/AboutData.js
+++ src/components/about/AboutData.js
@@ -1,9 +1,10 @@
 const imgProfilePic = require('../../../images/about/travis-and-lindsey-hoki.png');
+const imgFamily = require('../../../images/about/family.png');
+const imgWedding = require('../../../images/about/wedding.png');
 const imgGuitar = require('../../../images/about/guitar.png');
+const imgSkateboard = require('../../../images/about/skateboarding.png');
 const imgBallroom = require('../../../images/about/ballroom.png');
 const imgBreakdance = require('../../../images/about/breakdance.png');
-const imgWedding = require('../../../images/about/wedding.png');
-const imgSkateboard = require('../../../images/about/skateboarding.png');
 
 const age = new Date().getFullYear()-1990;
 
@@ -14,9 +15,19 @@ const AboutData = [
           img: imgProfilePic
       },
       {
-          title: 'Newlywed',
+          title: 'Family',
+          desc: 'My baby boy is my pride and joy. I love that kid so much.',
+          img: imgFamily
+      },
+      {
+          title: 'Wife',
           desc: 'I am married to my best friend and high school sweet heart Lindsey. We’ve had many adventures together in the last 9 years.',
           img: imgWedding
+      },
+      {
+          title: 'Skateboarding',
+          desc: 'Skating is a big stress reliever for me. I always have my board in the trunk of my car and use it whenever possible. Why walk anywhere when you can skateboard.',
+          img: imgSkateboard
       },
       {
           title: 'Guitar',
@@ -32,11 +43,6 @@ const AboutData = [
           title: 'Ballroom',
           desc: 'Though my wife and I don\'t dance as much as we did back in highschool. We still love to dance together.',
           img: imgBallroom
-      },
-      {
-          title: 'Skateboarding',
-          desc: 'Skating is a big stress reliever for me. I always have my board in the trunk of my car and use it whenever possible. Why walk anywhere when you can skateboard.',
-          img: imgSkateboard
       }
 ];
 
