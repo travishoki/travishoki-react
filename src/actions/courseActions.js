ff578682423efbb07d754ce394d0c7c2f400ec21
Travis Hoki <travishoki@Hoki-CB-Mac.hsd1.ut.comcast.net>
2020-09-24 00:53:46 -0600

    linting

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,41 +4,44 @@ import courseApi from '../api/mockCourseApi';
 import { beginAjaxCall, ajaxCallError } from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
-  return { type: types.LOAD_COURSES_SUCCESS, courses};
+	return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
 export function createCourseSuccess(course) {
-  return {type: types.CREATE_COURSE_SUCCESS, course};
+	return {type: types.CREATE_COURSE_SUCCESS, course};
 }
 
 export function updateCourseSuccess(course) {
-  return {type: types.UPDATE_COURSE_SUCCESS, course};
+	return {type: types.UPDATE_COURSE_SUCCESS, course};
 }
 
 export function loadCourses() {
-  return function(dispatch) {
-    dispatch(beginAjaxCall());
-    return courseApi.getAllCourses().then(courses => {
-      dispatch(loadCoursesSuccess(courses));
-    }).catch(error => {
-      throw(error);
-    });
-  };
+	return function(dispatch) {
+		dispatch(beginAjaxCall());
+
+		return courseApi.getAllCourses()
+			.then(courses => {
+				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch(error => {
+				throw(error);
+			});
+	};
 }
 
 export function saveContactForm(message) {
-  return function (dispatch, getState) {
-    dispatch(beginAjaxCall());
-    const { name, email, comment } = message;
-    const cmd = 'sendContactForm';
-    return axios.post('http://travishoki.com/portfolio-api.php', {
-            cmd, name, email, comment
-        })
-        .then(function () {
-        })
-        .catch(function (error) {
-            dispatch(ajaxCallError(error));
-            throw(error);
-        });
-  };
+	return function (dispatch) {
+		dispatch(beginAjaxCall());
+		const { name, email, comment } = message;
+		const cmd = 'sendContactForm';
+
+		return axios.post('http://travishoki.com/portfolio-api.php', {
+				cmd, name, email, comment
+			})
+			.then()
+			.catch(function (error) {
+				dispatch(ajaxCallError(error));
+				throw(error);
+			});
+	};
 }
