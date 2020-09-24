f544ebf4335f615ec80ec9b3b45908c79f795b99
Travis Hoki <travishoki@Hoki-CB-Mac.hsd1.ut.comcast.net>
2020-09-24 00:44:19 -0600

    import libraries in styles

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from './store/configureStore';
 import routes from './routes';
-import '../node_modules/font-awesome/less/font-awesome.less';
-import '../node_modules/bootstrap/less/bootstrap.less';
-import '../node_modules/toastr/toastr.less';
+import configureStore from './store/configureStore';
 import './styles/styles.less';
 
 const store = configureStore();
