99457bbb37d5db7c7b0f253e0f9f909a94b88cd0
Travis Hoki <travis.hoki@gmail.com>
2021-08-03 23:52:31 -0600

    webpack.base

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,19 +1,13 @@
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const path = require('path');
+
+const baseConfig = require('./webpack.base.js');
 
 const rootDir = path.resolve(__dirname, '../../');
 const srcPath = path.resolve(rootDir, 'src');
 
-module.exports = {
-    debug: true,
-    devtool: 'inline-source-map',
-    noInfo: false,
-    entry: [
-        'eventsource-polyfill', // necessary for hot reloading with IE
-        'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-        path.resolve(srcPath, 'index.js')
-    ],
-    target: 'web',
+module.exports = merge(baseConfig, {
     output: {
         path: path.resolve(rootDir, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
         publicPath: '/',
@@ -25,19 +19,5 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
-    ],
-    module: {
-        loaders: [
-            {test: /\.js$/, include: srcPath, loaders: ['babel']},
-            {test: /\.(jpg|png)$/, loaders: ['file']},
-            {test: /(\.less)$/, loaders: ['style', 'css', 'less']},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-            //font-awesome
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
-        ]
-    }
-};
+    ]
+});
