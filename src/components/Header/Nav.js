e2c07d7bbc35cc9326a20790927731d199de5958
Travis Hoki <travis.hoki@gmail.com>
2021-08-03 21:10:48 -0600

    Nav hook

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,64 +1,46 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 
-class Nav extends Component {
-	constructor(props) {
-		super(props);
+const Nav = ({
+	location,
+}) => {
+	const [ open, setOpen ] = useState(false);
 
-		this.state = {
-			open: false,
-		};
+	useEffect(() => {
+		close();
+	}, [ location ]);
 
-		this.toggleOpen = this.toggleOpen.bind(this);
-		this.close = this.close.bind(this);
-	}
+	const close = () => setOpen(false);
+	const toggleOpen = () => setOpen(!open);
 
-	componentDidUpdate(prevProps) {
-		const hasRouteChanged = this.props.location.pathname !== prevProps.location.pathname;
+	return (
+		<Fragment>
+			<div className="menu-btn">
+				<i
+					className="fa fa-bars mobileMenuIcon"
+					onClick={toggleOpen}
+				/>
+			</div>
 
-		if (hasRouteChanged) {
-			this.close();
-		}
-	}
-
-	close() {
-		this.setState({ open: false });
-	}
-
-	toggleOpen() {
-		this.setState({ open: !this.state.open });
-	}
-
-	render() {
-		return (
-			<Fragment>
-				<div className="menu-btn">
-					<i
-						className="fa fa-bars mobileMenuIcon"
-						onClick={this.toggleOpen}
-					/>
-				</div>
-
-				<ul
-					className={classnames(
-						'nav',
-						{
-							open: this.state.open,
-						}
-					)}
-				>
-					<li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
-					<li><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
-					<li><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
-					<li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-					<li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
-				</ul>
-			</Fragment>
-		);
-	}
-}
+			<ul
+				className={classnames(
+					'nav',
+					{
+						open,
+					}
+				)}
+			>
+				<li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+				<li><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
+				<li><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
+				<li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+				<li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+			</ul>
+		</Fragment>
+	);
+};
 
 Nav.propTypes = {
     history: PropTypes.object,
