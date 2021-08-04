dc5456cd4d4e4d2bdf620af5a4d5f311375b3ae8
Travis Hoki <travis.hoki@gmail.com>
2021-08-03 21:22:05 -0600

    refactor BackToTop

--- src/components/Footer/BackToTop.js
+++ src/components/Footer/BackToTop.js
@@ -1,47 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { scrollWindow } from './Footer.helpers';
 
 const DELAY_IN_MS = 16.66;
 const SCROLL_STEPS_IN_PX = 50;
 
-class BackToTop extends Component {
-    constructor(props) {
-        super(props);
+const BackToTop = () => {
+    const scrollToTop = () => {
+        const timer = setInterval(() => {
+            if (window.pageYOffset === 0) {
+                clearInterval(timer);
+            }
+    
+            const yPosition = window.pageYOffset - SCROLL_STEPS_IN_PX;
+    
+            scrollWindow(0, yPosition);
+        }, DELAY_IN_MS);
+    };
 
-		this.state = {
-			intervalId: null,
-		};
-
-        this.scrollToTop = this.scrollToTop.bind(this);
-    }
-
-    scrollStep(){
-        if (window.pageYOffset === 0) {
-            clearInterval(this.state.intervalId);
-        }
-
-		const yPosition = window.pageYOffset - SCROLL_STEPS_IN_PX;
-
-        scrollWindow(0, yPosition);
-    }
-
-    scrollToTop(){
-        const intervalId = setInterval(() => {
-			this.scrollStep();
-		}, DELAY_IN_MS);
-
-        this.setState({ intervalId });
-    }
-
-    render() {
-        return (
-            <i
-				className="circ-sm fa fa-angle-up"
-				onClick={this.scrollToTop}
-			/>
-        );
-    }
-}
+    return (
+        <i
+            className="circ-sm fa fa-angle-up"
+            onClick={scrollToTop}
+        />
+    );
+};
 
 export default BackToTop;
