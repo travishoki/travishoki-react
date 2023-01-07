26efdd021a2c443b4940691855f2df302f223ae6
Travis Hoki <travis.hoki@gmail.com>
2023-01-06 20:22:22 -0700

    hok skateboards breakdown

--- src/pages/HomePage/HokiSkateboards/Content.js
+++ src/pages/HomePage/HokiSkateboards/Content.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import ImgHokiSkateboardsLogo from "~images/home/hoki-skateboards.jpg";
 import ImgYouShouldAlwaysSkateboard from "~images/home/you-should-always-skateboard.jpg";
+
 import Title from "./Title";
 
 const Content = () => (
