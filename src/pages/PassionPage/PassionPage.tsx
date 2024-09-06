fbb7b16e9e6f08cdc2f794c2320eb3f386f8e8b0
Travis Hoki <travis.hoki@gmail.com>
2024-09-06 13:23:13 -0600

    styling passion

--- src/pages/PassionPage/PassionPage.tsx
+++ src/pages/PassionPage/PassionPage.tsx
@@ -4,10 +4,8 @@ import HokiSkateboards from './HokiSkateboards/HokiSkateboards';
 
 const PassionPage = () => (
 	<div className="passion" id="container">
-		<div className="boxed boxed-md">
-			<h1>My Passions</h1>
-			<HokiSkateboards />
-		</div>
+		<h1>My Passions</h1>
+		<HokiSkateboards />
 	</div>
 );
 
