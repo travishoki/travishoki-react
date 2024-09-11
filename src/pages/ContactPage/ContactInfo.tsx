d635d91e75f1ace1424877da6175f148058f17ac
Travis Hoki <travis.hoki@gmail.com>
2024-09-11 15:20:19 -0600

    update contact page info

--- src/pages/ContactPage/ContactInfo.tsx
+++ src/pages/ContactPage/ContactInfo.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 
 const ContactInfo = () => (
 	<div className="contact-info">
-		<p>Contact me with comments or critiques!</p>
-		<p>801-691-2373</p>
-		<p>travis.hoki@gmail.com</p>
-		<p>Based out of Pleasant Grove, UT</p>
-		<p>Open to remove work.</p>
+		<div className="block">
+			<p>Travis Hoki</p>
+			<p>801-691-2373</p>
+			<p>travis.hoki@gmail.com</p>
+			<p>Based out of Pleasant Grove, UT</p>
+		</div>
+
+		<div className="block">
+			<p>Open to remove work.</p>
+		</div>
+
+		<a
+			className="linked-link"
+			href="https://www.linkedin.com/in/travishoki"
+			rel="noreferrer"
+			target="_blank"
+			title="Travis's Linked In"
+		>
+			<img
+				className="linkedin-profile-image"
+				height="50"
+				src="https://media.licdn.com/dms/image/v2/D5603AQHVlroZx8i2gA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1707861433706?e=1731542400&v=beta&t=yYdc6o03EMz7gWcRTU6qaCz3KQdoDONcTVLu79DNj98"
+				width="50"
+			/>
+			Connect with me on:
+			<i className="fa fa-linkedin" />
+		</a>
 	</div>
 );
 
