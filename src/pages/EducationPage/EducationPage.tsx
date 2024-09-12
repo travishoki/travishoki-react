68247fe7d2162924a6c955d3cba326562163095d
Travis Hoki <travis.hoki@gmail.com>
2024-09-12 14:55:54 -0600

    education associates

--- src/pages/EducationPage/EducationPage.tsx
+++ src/pages/EducationPage/EducationPage.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 import imgUvu from '~images/global/uvu.png';
 
+import './EducationPage.scss';
+
 const EducationPage = () => (
 	<section className="white center education">
 		<h3>Utah Valley University</h3>
 		<img className="center" src={imgUvu} />
 
-		<p>Digital Media Major</p>
-		<p className="degree">Bachelor of Science</p>
-		<p>Internet Technology Emphasis</p>
+		<div className="text-block">
+			<p>Digital Media Major</p>
+			<p className="degree">Bachelor of Science</p>
+			<p>Internet Technology Emphasis</p>
+		</div>
+
+		<div className="text-block">
+			<p className="degree">Associate in Applied Science</p>
+			<p>Digital Communication Technology</p>
+		</div>
 	</section>
 );
 
