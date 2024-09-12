6069e51a07591ad9966462ffbbe0ba29fab86cf9
Travis Hoki <travis.hoki@gmail.com>
2024-09-12 15:29:07 -0600

    move images

--- src/pages/PassionPage/HokiSkateboards/Content.tsx
+++ src/pages/PassionPage/HokiSkateboards/Content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import ImgHokiSkateboardsLogo from '~images/home/hoki-skateboards.jpg';
-import ImgYouShouldAlwaysSkateboard from '~images/home/you-should-always-skateboard.jpg';
+import ImgHokiSkateboardsLogo from '~images/passion/hoki-skateboards.jpg';
+import ImgYouShouldAlwaysSkateboard from '~images/passion/you-should-always-skateboard.jpg';
 import {
 	CSS,
 	HTML,
