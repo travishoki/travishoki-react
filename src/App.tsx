2bcc8c41d6a54e1134544d3a4115b0850739dfee
Travis Hoki <travis.hoki@gmail.com>
2024-09-12 15:32:07 -0600

    cleanup comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-// This component handles the App template used on every page.
 import React, { ReactNode } from 'react';
-// import { connect } from 'react-redux';
 
 import Header from '~components/Header/Header';
 import Footer from '~components/Footer/Footer';
-// import { FormState } from '~reducers/initialState';
 import './styles/styles.scss';
 
 const App = ({ children }: AppProps) => (
@@ -17,12 +14,6 @@ const App = ({ children }: AppProps) => (
 
 type AppProps = {
 	children: ReactNode;
-	// loading: boolean;
 };
 
-// const mapStateToProps = (state: FormState) => ({
-// 	loading: state.ajaxCallsInProgress > 0,
-// });
-
 export default App;
-// export default connect(mapStateToProps)(App);
