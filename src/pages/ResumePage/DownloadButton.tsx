6e7727d1e1c3eba5f850853606d1a0309d7c2807
Travis Hoki <travis.hoki@gmail.com>
2024-09-13 10:20:10 -0600

    download-button styling

--- src/pages/ResumePage/DownloadButton.tsx
+++ src/pages/ResumePage/DownloadButton.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './DownloadButton.scss';
+
 const DownloadButton = () => (
 	<a
-		className="btn btn-primary btn-lg"
+		className="btn btn-primary btn-lg download-button"
 		href="http://travishoki.com/travis-hoki-resume.pdf"
 		rel="noreferrer"
 		target="_blank"
