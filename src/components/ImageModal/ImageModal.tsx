b61585e1d997dd888ca829370f9b90b259d0b7bd
Travis Hoki <travis.hoki@gmail.com>
2024-09-13 14:36:51 -0600

    image modal close

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -4,10 +4,10 @@ import './ImageModal.scss';
 
 export const ImageModal = ({ onClose, src }: ImageModalProps) => {
 	return (
-		<div className="image-modal">
+		<div className="image-modal" onClick={onClose}>
 			<div className="image-modal-inner">
 				<img src={src} />
-				<i className="fa fa-close close-icon" onClick={onClose} />
+				<i className="fa fa-close close-icon" />
 			</div>
 		</div>
 	);
