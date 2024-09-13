c39f0f254381a529d1e641dca1475504f714b3f0
Travis Hoki <travis.hoki@gmail.com>
2024-09-13 12:23:45 -0600

    cleanup store comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
-/*eslint-disable import/default*/
-// import React from 'react';
 import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
 
 import routes from './routes';
-// import configureStore from './store/configureStore.dev';
+
 import './styles/styles.scss';
-
-// const store = configureStore();
-
-// render(
-// 	<Provider store={store}>{routes}</Provider>,
-// 	document.getElementById('app'),
-// );
 
 render(routes, document.getElementById('app'));
