f0b1ad72f823b89925ef9bca82c6a1d9dfa47414
Travis Hoki <travis.hoki@gmail.com>
2024-09-17 23:50:08 -0600

    update home banner image

--- src/pages/HomePage/Banner/Banner.tsx
+++ src/pages/HomePage/Banner/Banner.tsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { SocialIcons } from '~components/SocialIcons/SocialIcons';
-import ImgProfilePic from '~images/global/travis-hoki.png';
+import ImgProfilePic from '~images/global/travis-hoki.jpg';
 
 import './Banner.scss';
 
 export const Banner = () => (
-	<div className="home-banner">
-		<div className="banner-cont">
+	<div className="boxed home-banner">
+		<img className="banner-pic" src={ImgProfilePic} />
+		<div>
 			<h1>Web Developer</h1>
 			<SocialIcons />
 			<Link className="btn btn-primary btn-lg" to="/contact">
 				Contact Me
 			</Link>
 		</div>
-
-		<div className="banner-bg" />
-
-		<img className="banner-pic" src={ImgProfilePic} />
 	</div>
 );
