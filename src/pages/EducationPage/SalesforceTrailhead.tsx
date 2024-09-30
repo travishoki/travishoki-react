39d5594915d3e7753ab5501a9c09e057b2ff20f5
Travis Hoki <travis.hoki@gmail.com>
2024-09-30 11:43:30 -0600

    trailhead responsive

--- src/pages/EducationPage/SalesforceTrailhead.tsx
+++ src/pages/EducationPage/SalesforceTrailhead.tsx
@@ -8,22 +8,21 @@ import './SalesforceTrailhead.scss';
 export const SalesforceTrailhead = () => (
 	<section className="gray ">
 		<div className="boxed trailhead">
-			<img src={imgRanger} />
-			<div className="trailhead-content">
-				<div className="earned">
-					<h2>Salesforce&apos;s Ranger Status</h2>
-					<p>Earned July 25, 2024</p>
-					<a
-						href="https://www.salesforce.com/trailblazer/nyzvvx5htc3badv2b1"
-						rel="noreferrer"
-						target="_blank"
-						title="Trailblazer"
-					>
-						<SalesforceIcon />
-						View my profile
-					</a>
-				</div>
-
+			<img className="trailhead-image" src={imgRanger} />
+			<div className="earned">
+				<h2>Salesforce&apos;s Ranger Status</h2>
+				<p>Earned July 25, 2024</p>
+				<a
+					href="https://www.salesforce.com/trailblazer/nyzvvx5htc3badv2b1"
+					rel="noreferrer"
+					target="_blank"
+					title="Trailblazer"
+				>
+					<SalesforceIcon />
+					View my profile
+				</a>
+			</div>
+			<div className="rank">
 				<h3>What are Trailblazer Ranks?</h3>
 				<p>
 					<a
@@ -38,7 +37,7 @@ export const SalesforceTrailhead = () => (
 					you complete earns you a badge and points that go towards your
 					Trailblazer Rank. Climb the ranks and learn new skills.
 				</p>
-			</div>
+			</div>{' '}
 		</div>
 	</section>
 );
