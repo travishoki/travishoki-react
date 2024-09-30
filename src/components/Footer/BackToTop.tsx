d98ca665aa951e48073cc8f9a3f1ae586123b471
Travis Hoki <travis.hoki@gmail.com>
2024-09-30 11:46:52 -0600

    scroll to top fix

--- src/components/Footer/BackToTop.tsx
+++ src/components/Footer/BackToTop.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
 
-import { scrollWindow } from './Footer.helpers';
-
-const DELAY_IN_MS = 16.66;
-const SCROLL_STEPS_IN_PX = 50;
-
 export const BackToTop = () => {
 	const scrollToTop = () => {
-		const timer = setInterval(() => {
-			if (window.pageYOffset === 0) {
-				clearInterval(timer);
-			}
-
-			const yPosition = window.pageYOffset - SCROLL_STEPS_IN_PX;
-
-			scrollWindow(0, yPosition);
-		}, DELAY_IN_MS);
+		window.scrollTo({ behavior: 'smooth', top: 0 });
 	};
 
 	return <i className="circ-sm fa fa-angle-up" onClick={scrollToTop} />;
