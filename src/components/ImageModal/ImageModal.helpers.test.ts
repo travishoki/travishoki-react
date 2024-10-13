c5bacc88bd4ed965e9649cec99bb26fa95d5364e
Travis Hoki <travis.hoki@gmail.com>
2024-10-12 22:20:29 -0600

    helper tests

--- src/components/ImageModal/ImageModal.helpers.test.ts
+++ src/components/ImageModal/ImageModal.helpers.test.ts
@@ -23,3 +23,23 @@ describe('getHeightFromWidth', () => {
 		expect(result).toEqual(expectedResult);
 	});
 });
+
+describe('getLargerNum', () => {
+	test('getLargerNum - width', () => {
+		const height = 200;
+		const width = 100;
+		const result = helpers.getSmallerNum(width, height);
+		const expectedResult = width;
+
+		expect(result).toEqual(expectedResult);
+	});
+
+	test('getSmallerNum - height', () => {
+		const height = 100;
+		const width = 200;
+		const result = helpers.getSmallerNum(width, height);
+		const expectedResult = height;
+
+		expect(result).toEqual(expectedResult);
+	});
+});
