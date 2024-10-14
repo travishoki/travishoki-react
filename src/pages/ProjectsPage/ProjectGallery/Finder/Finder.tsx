d63b0c1e2fa461eec6a15ebf5b7ff08bab96bef4
Travis Hoki <travis.hoki@gmail.com>
2024-10-13 19:33:03 -0600

    breakout finder

---
+++ src/pages/ProjectsPage/ProjectGallery/Finder/Finder.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { SearchForm } from './SearchForm/SearchForm';
+import { ViewControl } from './ViewControl/ViewControl';
+import { Filter } from './Filter/Filter';
+import { TechFilterType, TechKeys } from '~const/Tech.const';
+import { createProjectsPageUrl } from '../ProjectGallery.helpers';
+
+const PROJECTS_PAGE_URL = '/projects';
+
+export const Finder = ({
+	filter,
+	filterList,
+	filterTerm,
+	grid,
+	onClearFilter,
+	onClearSearchTerm,
+	projectsLength,
+	searchTerm,
+	setFilter,
+	setFilterTerm,
+	setGrid,
+	setSearchTerm,
+}: FinderProps) => {
+	const navigate = useNavigate();
+
+	const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+	const toggleView = () => {
+		setGrid(!grid);
+	};
+
+	const onSelectFilter = (filterProp: TechFilterType) => {
+		const newFilter = filter === filterProp ? null : filterProp;
+		const newLocation = createProjectsPageUrl(
+			PROJECTS_PAGE_URL,
+			newFilter,
+			searchTerm,
+		);
+
+		navigate(newLocation);
+
+		setFilter(newFilter);
+		setFilterTerm(newFilter);
+	};
+
+	const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newSearchTerm = e.currentTarget.value;
+
+		const newLocation = createProjectsPageUrl(
+			PROJECTS_PAGE_URL,
+			filter,
+			newSearchTerm,
+		);
+
+		navigate(newLocation);
+
+		setSearchTerm(newSearchTerm);
+	};
+
+	const onChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newFilter = e.target.value as TechFilterType;
+		const newLocation = createProjectsPageUrl(PROJECTS_PAGE_URL, newFilter);
+
+		navigate(newLocation);
+
+		setFilterTerm(newFilter);
+	};
+
+	const toggleOpenFilter = () => {
+		setIsFilterOpen(!isFilterOpen);
+	};
+
+	const onClearAndCloseFilter = () => {
+		onClearFilter();
+		toggleOpenFilter();
+	};
+
+	const showResultsCount = (filter || searchTerm) && projectsLength > 0;
+
+	return (
+		<div className="find-section">
+			<p className="center">
+				Click on the icons below to filter my work by different technologies
+			</p>
+
+			<Filter
+				filter={filter}
+				filterList={filterList}
+				filterTerm={filterTerm}
+				isFilterOpen={isFilterOpen}
+				onChangeFilter={onChangeFilter}
+				onClearAndCloseFilter={onClearAndCloseFilter}
+				onSelectFilter={onSelectFilter}
+				toggleOpenFilter={toggleOpenFilter}
+			/>
+
+			<div className="clearfix" />
+
+			<SearchForm
+				onChangeSearch={onChangeSearch}
+				onClearSearchTerm={onClearSearchTerm}
+				searchTerm={searchTerm}
+			/>
+
+			{showResultsCount && (
+				<p className="results-count">Results: {projectsLength}</p>
+			)}
+
+			<ViewControl
+				grid={grid}
+				isVisible={projectsLength > 0}
+				toggleView={toggleView}
+			/>
+		</div>
+	);
+};
+
+type FinderProps = {
+	filter: TechFilterType;
+	filterList: (keyof TechKeys)[];
+	filterTerm: TechFilterType;
+	grid: boolean;
+	onClearFilter: () => void;
+	onClearSearchTerm: () => void;
+	projectsLength: number;
+	searchTerm: string;
+	setFilter: (filter: TechFilterType) => void;
+	setFilterTerm: (filterTerm: TechFilterType) => void;
+	setGrid: (grid: boolean) => void;
+	setSearchTerm: (searchTerm: string) => void;
+};
--- src/pages/ProjectsPage/ProjectGallery/ProjectGallery.tsx
+++ src/pages/ProjectsPage/ProjectGallery/ProjectGallery.tsx
@@ -12,11 +12,9 @@ import {
 } from './ProjectGallery.helpers';
 import { NoResults } from './NoResults/NoResults';
 import { Projects } from './Projects/Projects';
-import { SearchForm } from './SearchForm/SearchForm';
-import { ViewControl } from './ViewControl/ViewControl';
-import { Filter } from './Filter/Filter';
 
 import './ProjectGallery.scss';
+import { Finder } from './Finder/Finder';
 
 const PROJECTS_PAGE_URL = '/projects';
 
@@ -43,40 +41,7 @@ export const ProjectGallery = ({
 	const [filter, setFilter] = useState<TechFilterType>(initialFilter);
 	const [filterTerm, setFilterTerm] = useState<TechFilterType>(initialFilter);
 	const [grid, setGrid] = useState(true);
-	const [isFilterOpen, setIsFilterOpen] = useState(false);
 	const [searchTerm, setSearchTerm] = useState(initialSearch);
-
-	const toggleView = () => {
-		setGrid(!grid);
-	};
-
-	const onSelectFilter = (filterProp: TechFilterType) => {
-		const newFilter = filter === filterProp ? null : filterProp;
-		const newLocation = createProjectsPageUrl(
-			PROJECTS_PAGE_URL,
-			newFilter,
-			searchTerm,
-		);
-
-		navigate(newLocation);
-
-		setFilter(newFilter);
-		setFilterTerm(newFilter);
-	};
-
-	const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const newSearchTerm = e.currentTarget.value;
-
-		const newLocation = createProjectsPageUrl(
-			PROJECTS_PAGE_URL,
-			filter,
-			newSearchTerm,
-		);
-
-		navigate(newLocation);
-
-		setSearchTerm(newSearchTerm);
-	};
 
 	const onClearFilter = () => {
 		const newLocation = createProjectsPageUrl(PROJECTS_PAGE_URL);
@@ -95,26 +60,7 @@ export const ProjectGallery = ({
 		setSearchTerm('');
 	};
 
-	const onChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const newFilter = e.target.value as TechFilterType;
-		const newLocation = createProjectsPageUrl(PROJECTS_PAGE_URL, newFilter);
-
-		navigate(newLocation);
-
-		setFilterTerm(newFilter);
-	};
-
-	const toggleOpenFilter = () => {
-		setIsFilterOpen(!isFilterOpen);
-	};
-
-	const onClearAndCloseFilter = () => {
-		onClearFilter();
-		toggleOpenFilter();
-	};
-
 	const projects = filterProjects(projectsData, filter, searchTerm);
-	const showResultsCount = (filter || searchTerm) && projects.length > 0;
 
 	return (
 		<div className="projects" id="container">
@@ -126,41 +72,20 @@ export const ProjectGallery = ({
 			>
 				<h1>Projects</h1>
 
-				<div className="find-section">
-					<p className="center">
-						Click on the icons below to filter my work by different technologies
-					</p>
-
-					<Filter
-						filter={filter}
-						filterList={filterList}
-						filterTerm={filterTerm}
-						isFilterOpen={isFilterOpen}
-						onChangeFilter={onChangeFilter}
-						onClearAndCloseFilter={onClearAndCloseFilter}
-						onSelectFilter={onSelectFilter}
-						toggleOpenFilter={toggleOpenFilter}
-					/>
-
-					<div className="clearfix" />
-
-					<SearchForm
-						onChangeSearch={onChangeSearch}
-						onClearSearchTerm={onClearSearchTerm}
-						searchTerm={searchTerm}
-					/>
-
-					{showResultsCount && (
-						<p className="results-count">Results: {projects.length}</p>
-					)}
-
-					<ViewControl
-						grid={grid}
-						isVisible={projects.length > 0}
-						toggleView={toggleView}
-					/>
-				</div>
-
+				<Finder
+					filter={filter}
+					filterList={filterList}
+					filterTerm={filterTerm}
+					grid={grid}
+					onClearFilter={onClearFilter}
+					onClearSearchTerm={onClearSearchTerm}
+					projectsLength={projects.length}
+					searchTerm={searchTerm}
+					setFilter={setFilter}
+					setFilterTerm={setFilterTerm}
+					setGrid={setGrid}
+					setSearchTerm={setSearchTerm}
+				/>
 				<Projects grid={grid} projectLink={projectLink} projects={projects} />
 
 				<NoResults
