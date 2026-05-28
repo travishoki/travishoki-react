81a28b33532bb43d042859391c42f93b15e9dbdf
Travis Hoki <travis.hoki@gmail.com>
2026-05-28 11:14:35 -0600

    experience tech divider

--- src/pages/ResumePage/Experience.tsx
+++ src/pages/ResumePage/Experience.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { ExperienceItem } from './ExperienceItem';
 import { RESUME_DATA } from './ResumePage.data';
+import { TechDivider } from './TechDivider';
 
 export const Experience = () => (
 	<section className="experience">
@@ -9,9 +10,16 @@ export const Experience = () => (
 			<h2>Experience</h2>
 
 			<ul className="staggered">
-				{RESUME_DATA.map((job, index) => (
-					<ExperienceItem key={index} {...job} />
-				))}
+				{RESUME_DATA.map((job, index) => {
+					const showTechDivider = index === RESUME_DATA.length - 3;
+
+					return (
+						<Fragment key={index}>
+							{showTechDivider && <TechDivider />}
+							<ExperienceItem {...job} />
+						</Fragment>
+					);
+				})}
 			</ul>
 		</div>
 	</section>
---
+++ src/pages/ResumePage/TechDivider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './TechDivider.scss';
+
+export const TechDivider = () => {
+	return (
+		<li className="tech-divider">
+			<h2>Pre Tech Experience</h2>
+		</li>
+	);
+};
