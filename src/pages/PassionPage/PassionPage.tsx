1e3e20ac1dd8c6c3892cca13dcf2f62b196ad8cf
Travis Hoki <travis.hoki@gmail.com>
2026-05-27 18:37:28 -0600

    update title

--- src/pages/PassionPage/PassionPage.tsx
+++ src/pages/PassionPage/PassionPage.tsx
@@ -4,7 +4,7 @@ import { HokiSkateboards } from './HokiSkateboards/HokiSkateboards';
 
 export const PassionPage = () => (
 	<div className="passion" id="container">
-		<h1>My Passions</h1>
+		<h1>Passion Project</h1>
 		<HokiSkateboards />
 	</div>
 );
