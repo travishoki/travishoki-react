aa2d007c0069a27aad11e8c185ab81a35956ad6e
Travis Hoki <travis.hoki@gmail.com>
2026-05-28 12:19:12 -0600

    styling

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import ImgProfilePic from '~images/global/travis-hoki-2.jpg';
 
 import { ContactInfo } from './ContactInfo';
-import './ContactPage.scss';
 
 export const ContactPage = () => (
 	<div className="contact" id="container">
 		<div className="boxed boxed-lg">
 			<h1>Contact Me</h1>
 
-			<div className="contact-content row">
+			<div className="row">
 				<div className="col-sm-6">
 					<img className="content-image" src={ImgProfilePic} />
 				</div>
