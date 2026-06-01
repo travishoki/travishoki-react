6028824a5f55911c8f86e568809ab2404d57b6c9
Travis Hoki <travis.hoki@gmail.com>
2026-06-01 11:22:26 -0600

    volunteer styling

--- src/pages/HomePage/Volunteer/Volunteer.tsx
+++ src/pages/HomePage/Volunteer/Volunteer.tsx
@@ -9,7 +9,15 @@ export const Volunteer = () => (
 
 		<img className="center" height="200" src={imgScoutingAmerica} width="177" />
 		<h4>Volunteering in Cub Scouts Since 2025</h4>
-		<p>Bear&apos;s Assistant Den Leader since spring of 2025</p>
-		<p>Tentative Webelos Den Leader Spring of 2026</p>
+		<div className="volunteer-roles">
+			<div>
+				<p>Bear&apos;s Assistant Den Leader</p>
+				<p>since spring of 2025</p>
+			</div>
+			<div>
+				<p>Tentative Webelos Den Leader</p>
+				<p>Spring of 2026</p>
+			</div>
+		</div>
 	</section>
 );
