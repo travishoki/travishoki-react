45fd56bb1d6bbc64293ed224f3aaa5eb211e91f0
Travis Hoki <travis.hoki@gmail.com>
2026-06-02 08:53:18 -0600

    prefer email but also linked in

--- src/pages/ContactPage/LinkedInLink/LinkedInLink.tsx
+++ src/pages/ContactPage/LinkedInLink/LinkedInLink.tsx
@@ -5,20 +5,26 @@ import ImgProfilePic from '~images/global/travis-hoki.jpg';
 import './LinkedInLink.scss';
 
 export const LinkedInLink = () => (
-	<a
-		className="linked-link"
-		href="https://www.linkedin.com/in/travishoki"
-		rel="noreferrer"
-		target="_blank"
-		title="Travis's Linked In"
-	>
-		<img
-			className="linkedin-profile-image"
-			height="50"
-			src={ImgProfilePic}
-			width="50"
-		/>
-		<p>Connect with me on</p>
-		<i className="fa fa-linkedin" />
-	</a>
+	<>
+		<p className="preference-text">
+			I prefer being contacted by email, but also
+		</p>
+
+		<a
+			className="linked-link"
+			href="https://www.linkedin.com/in/travishoki"
+			rel="noreferrer"
+			target="_blank"
+			title="Travis's Linked In"
+		>
+			<img
+				className="linkedin-profile-image"
+				height="50"
+				src={ImgProfilePic}
+				width="50"
+			/>
+			<p>Connect with me on</p>
+			<i className="fa fa-linkedin" />
+		</a>
+	</>
 );
