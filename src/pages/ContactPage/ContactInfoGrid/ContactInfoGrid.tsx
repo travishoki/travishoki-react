71569c4a50d9333a50ff6155ee90257955dad36c
Travis Hoki <travis.hoki@gmail.com>
2026-06-02 07:45:48 -0600

    contact page reworking

--- src/pages/ContactPage/ContactInfoGrid/ContactInfoGrid.tsx
+++ src/pages/ContactPage/ContactInfoGrid/ContactInfoGrid.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { CONTACT_INFO } from '../ContactInfo.data';
-import { ContactInfoItem } from '../ContactInfoItem/ContactInfoItem';
 import './ContactInfoGrid.scss';
 
 export const ContactInfoGrid = () => (
 	<div className="contact-info-grid">
-		{CONTACT_INFO.map((item) => (
-			<ContactInfoItem key={item.id} {...item} />
-		))}
+		<div className="contact-info-item">
+			<i className="fa fa-envelope contact-info-icon" />
+			<p>
+				<a href="mailto:travis.hoki@gmail.com">travis.hoki@gmail.com</a>
+			</p>
+			<p>(Preferred)</p>
+		</div>
+
+		<div className="contact-info-item">
+			<i className="fa fa-phone contact-info-icon" />
+			<p>
+				<a href="tel:+18016912373">801-691-2373</a>
+			</p>
+		</div>
+
+		<div className="contact-info-item">
+			<i className="fa fa-map-marker contact-info-icon" />
+			<p>Pleasant Grove, Utah</p>
+			<p>United States</p>
+			<p>Mountain Time (MT)</p>
+		</div>
+
+		<div className="contact-info-item">
+			<i className="fa fa-clock-o contact-info-icon" />
+			<p>Open to remote work</p>
+		</div>
 	</div>
 );
