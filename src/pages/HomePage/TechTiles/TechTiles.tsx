cb44ec30e6bfd525708381f2ae3ef191715499c5
Travis Hoki <travis.hoki@gmail.com>
2026-06-01 19:09:08 -0600

    tech titles on home page

--- src/pages/HomePage/TechTiles/TechTiles.tsx
+++ src/pages/HomePage/TechTiles/TechTiles.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { TechIcon } from '~components/TechIcon/TechIcon';
-import { TechStrings } from '~const/Tech.const';
+import { TechListSwitcher } from '~components/TechListSwitcher/TechListSwitcher';
 
 import { DISPLAYED_LIST } from '../HomePage.data';
 import './TechTiles.scss';
 
 export const TechTiles = () => (
 	<section className="white tech-tiles">
-		<ul>
-			{DISPLAYED_LIST.map((tech, index) => (
-				<li key={index}>
-					<Link title={TechStrings[tech]} to={`/projects/${tech}`}>
-						<TechIcon name={tech} />
-					</Link>
-				</li>
-			))}
-		</ul>
+		<TechListSwitcher
+			header="Tech I have expereice with"
+			list={DISPLAYED_LIST}
+		/>
 	</section>
 );
