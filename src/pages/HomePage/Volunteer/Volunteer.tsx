d7aa38797fb7874026dc77b535b9a4599f5b7f7f
Travis Hoki <travis.hoki@gmail.com>
2026-06-04 10:49:56 -0600

    volunteer using css bootstrap

--- src/pages/HomePage/Volunteer/Volunteer.tsx
+++ src/pages/HomePage/Volunteer/Volunteer.tsx
@@ -8,13 +8,13 @@ export const Volunteer = () => (
 		<h2>Scouting America</h2>
 
 		<img className="center" height="200" src={imgScoutingAmerica} width="177" />
-		<h3>Volunteering in Cub Scouts Since 2025</h3>
-		<div className="volunteer-roles">
-			<div>
+		<h3 className="mb-2">Volunteering in Cub Scouts Since 2025</h3>
+		<div className="row justify-content-center">
+			<div className="col-auto">
 				<p>Bear&apos;s Assistant Den Leader</p>
 				<p>Since Spring of 2025</p>
 			</div>
-			<div>
+			<div className="col-auto">
 				<p>Webelos&apos; Den Leader</p>
 				<p>Summer of 2026</p>
 			</div>
