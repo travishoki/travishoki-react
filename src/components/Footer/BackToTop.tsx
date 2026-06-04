129b4a3a5fd53c35ae5c62f76da0b1527f373581
Travis Hoki <travis.hoki@gmail.com>
2026-06-04 12:57:17 -0600

    back to top fixed position

--- src/components/Footer/BackToTop.tsx
+++ src/components/Footer/BackToTop.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import './BackToTop.scss';
 
 export const BackToTop = () => {
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		const onScroll = () => setVisible(window.scrollY > 200);
+
+		window.addEventListener('scroll', onScroll);
+
+		return () => window.removeEventListener('scroll', onScroll);
+	}, []);
+
 	const scrollToTop = () => {
 		window.scrollTo({ behavior: 'smooth', top: 0 });
 	};
 
-	return <i className="circ-sm fa fa-angle-up" onClick={scrollToTop} />;
+	return (
+		<button
+			className={`back-to-top ${visible ? 'back-to-top--visible' : ''}`}
+			onClick={scrollToTop}
+		>
+			<i className="fa fa-chevron-up" />
+		</button>
+	);
 };
