693545a41294489100304da7d5ffa2f3fa2ccf8d
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 05:22:00 -0600

    remove linkedIn label

--- src/pages/ContactPage/LinkedInLink/LinkedInLink.tsx
+++ src/pages/ContactPage/LinkedInLink/LinkedInLink.tsx
@@ -5,19 +5,15 @@ import ImgProfilePic from '~images/global/travis-hoki.jpg';
 import './LinkedInLink.scss';
 
 export const LinkedInLink = () => (
-	<>
-		<p className="preference-text">Connect with me on LinkedIn</p>
-
-		<a
-			className="linked-link"
-			href="https://www.linkedin.com/in/travishoki"
-			rel="noreferrer"
-			target="_blank"
-			title="Travis's Linked In"
-		>
-			<img height="50" src={ImgProfilePic} width="50" />
-			<p>Connect with me on</p>
-			<i className="fa fa-linkedin" />
-		</a>
-	</>
+	<a
+		className="linked-link"
+		href="https://www.linkedin.com/in/travishoki"
+		rel="noreferrer"
+		target="_blank"
+		title="Travis's Linked In"
+	>
+		<img height="50" src={ImgProfilePic} width="50" />
+		<p>Connect with me on</p>
+		<i className="fa fa-linkedin" />
+	</a>
 );
