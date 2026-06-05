e73a1f8dc6387afc3da5f2135869e87b155a4130
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 16:30:19 -0600

    contributions either ul or p

--- src/pages/SingleProjectPage/ProjectContent/ProjectInfoSection/Contributions/Contributions.tsx
+++ src/pages/SingleProjectPage/ProjectContent/ProjectInfoSection/Contributions/Contributions.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 
 import { ProjectType } from '../../../../ProjectsPage/ProjectsPage.types';
+import { getFirstParagraph } from '~helpers/arrays';
 
 type ContributionsProps = Pick<ProjectType, 'contributions'>;
 
 export const Contributions = ({ contributions }: ContributionsProps) => {
 	if (!contributions || contributions.length === 0) return null;
 
+	const isArray = Array.isArray(contributions) && contributions.length > 1;
+
 	return (
 		<>
 			<p className="label">Contributions:</p>
 			<div className="answer-section">
-				{Array.isArray(contributions) ? (
+				{isArray ? (
 					<ul className="disc answer-section-list">
 						{contributions.map((contribution, index) => (
 							<li key={index}>{contribution}</li>
 						))}
 					</ul>
 				) : (
-					<p>{contributions}</p>
+					<p className="indent">{getFirstParagraph(contributions)}</p>
 				)}
 			</div>
 		</>
