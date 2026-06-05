fb764d102952ef1fb3b451a4242173f055b62a15
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 16:05:06 -0600

    fix tests hopefully

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,17 @@ module.exports = {
 	],
 	coverageReporters: ['json'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|webp|svg|css|scss|sass)$':
+			'<rootDir>/testing/fileMock.ts',
+		'^~actions/(.*)$': '<rootDir>/src/redux/actions/$1',
+		'^~components/(.*)$': '<rootDir>/src/components/$1',
+		'^~const/(.*)$': '<rootDir>/src/const/$1',
+		'^~helpers/(.*)$': '<rootDir>/src/helpers/$1',
+		'^~images/(.*)$': '<rootDir>/src/images/$1',
+		'^~reducers/(.*)$': '<rootDir>/src/redux/reducers/$1',
+		'^~svg/(.*)$': '<rootDir>/src/svg/$1',
+	},
 	preset: 'ts-jest',
 	setupFilesAfterEnv: ['<rootDir>/testing/setupTests.ts'],
 	testEnvironment: 'node',
