a7b1b276f64cfad4a9580d56432fb892729efe9b
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 06:18:16 -0600

    ProjectInfoSection move logic out of tsx

--- src/pages/ProjectsPage/ProjectItemList/ProjectInfoSection/ProjectInfoSection.tsx
+++ src/pages/ProjectsPage/ProjectItemList/ProjectInfoSection/ProjectInfoSection.tsx
@@ -19,62 +19,66 @@ export const ProjectInfoSection = ({
 	description,
 	learned,
 	techs,
-}: ProjectInfoSectionProps) => (
-	<div className="col-sm-6">
-		<img className="logo center" src={logos(`./${companySlug}.jpg`)} />
+}: ProjectInfoSectionProps) => {
+	const descriptionArray = Array.isArray(description)
+		? description
+		: [description];
 
-		{date && (
-			<>
-				<p className="label">Date Range:</p>
-				<div className="answer-section">
-					<p>{date}</p>
-				</div>
-			</>
-		)}
+	return (
+		<div className="col-sm-6">
+			<img className="logo center" src={logos(`./${companySlug}.jpg`)} />
 
-		{description && (
-			<>
-				<p className="label">Description:</p>
-				<div className="answer-section">
-					{(Array.isArray(description) ? description : [description]).map(
-						(paragraph, index) => (
+			{date && (
+				<>
+					<p className="label">Date Range:</p>
+					<div className="answer-section">
+						<p>{date}</p>
+					</div>
+				</>
+			)}
+
+			{description.length && (
+				<>
+					<p className="label">Description:</p>
+					<div className="answer-section">
+						{descriptionArray.map((paragraph, index) => (
 							<p key={index} className="indent">
 								{paragraph}
 							</p>
-						),
-					)}
-				</div>
-			</>
-		)}
+						))}
+					</div>
+				</>
+			)}
 
-		{contributions && contributions.length > 0 && (
-			<>
-				<p className="label">Contributions:</p>
-				<div className="answer-section">
-					{Array.isArray(contributions) ? (
-						<ul className="disc answer-section-list">
-							{contributions.map((contribution, index) => (
-								<li key={index}>{contribution}</li>
-							))}
-						</ul>
-					) : (
-						<p>{contributions}</p>
-					)}
-				</div>
-			</>
-		)}
+			{contributions && contributions.length > 0 && (
+				<>
+					<p className="label">Contributions:</p>
+					<div className="answer-section">
+						{Array.isArray(contributions) ? (
+							<ul className="disc answer-section-list">
+								{contributions.map((contribution, index) => (
+									<li key={index}>{contribution}</li>
+								))}
+							</ul>
+						) : (
+							<p>{contributions}</p>
+						)}
+					</div>
+				</>
+			)}
 
-		{learned && (
-			<>
-				<p className="label">What I Learned:</p>
-				<div className="answer-section">
-					<p>{learned}</p>
-				</div>
-			</>
-		)}
+			{learned && (
+				<>
+					<p className="label">What I Learned:</p>
+					<div className="answer-section">
+						<p>{learned}</p>
+					</div>
+				</>
+			)}
 
-		{techs && techs.length > 0 && (
-			<TechListCatalogue initialExpanded list={techs} />
-		)}
-	</div>
-);
+			{techs && techs.length > 0 && (
+				<TechListCatalogue initialExpanded list={techs} />
+			)}
+		</div>
+	);
+};
