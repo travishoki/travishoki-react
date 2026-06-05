2dc09035595b0a15335e862ccf8096f8175e1e05
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 06:11:02 -0600

    scss in adjacent file to tsx

--- src/components/TechCatalogue/TechCatalogueItem/TechCatalogueItem.tsx
+++ src/components/TechCatalogue/TechCatalogueItem/TechCatalogueItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './TechCatalogueItem.scss';
+
 import { TechCatalogueItemData } from '../TechCatalogue.types';
 
 type TechCatalogueItemProps = TechCatalogueItemData & {
