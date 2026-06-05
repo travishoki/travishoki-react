0cfd0bf56169eef256bc86cad0f5667146867b2d
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 17:00:22 -0600

    missing images

--- src/pages/SingleProjectPage/ProjectContent/ProjectImage/ProjectImage.tsx
+++ src/pages/SingleProjectPage/ProjectContent/ProjectImage/ProjectImage.tsx
@@ -5,9 +5,25 @@ import './ProjectImage.scss';
 
 const images = require.context('../../../../images/websites', true);
 
-export const ProjectImage = ({ itemKey }: ProjectImageProps) => (
-	<img className="project-img" src={images(`./${itemKey}/lg.jpg`)} />
-);
+const fallbackSrc = images('./missing-images/lg.jpg');
+
+export const ProjectImage = ({ itemKey }: ProjectImageProps) => {
+	const onError = (event: React.SyntheticEvent<HTMLImageElement>) => {
+		const img = event.currentTarget;
+
+		if (img.src !== fallbackSrc) {
+			img.src = fallbackSrc;
+		}
+	};
+
+	return (
+		<img
+			className="project-img"
+			onError={onError}
+			src={images(`./${itemKey}/lg.jpg`)}
+		/>
+	);
+};
 
 type ProjectImageProps = {
 	itemKey: ProjectType['itemKey'];
