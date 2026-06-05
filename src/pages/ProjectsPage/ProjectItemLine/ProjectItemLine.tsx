92e1a8c43a0c3de0000100a85c4e64d817acb7ae
Travis Hoki <travis.hoki@gmail.com>
2026-06-05 13:38:58 -0600

    project item line data

--- src/pages/ProjectsPage/ProjectItemLine/ProjectItemLine.tsx
+++ src/pages/ProjectsPage/ProjectItemLine/ProjectItemLine.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { firstItem } from '~helpers/arrays';
 import { ProjectType } from '../ProjectsPage.data';
 import { ProductImageWithPopup } from './ProductImageWithPopup/ProductImageWithPopup';
 import './ProjectItemLine.scss';
@@ -7,21 +8,32 @@ import './ProjectItemLine.scss';
 export const ProjectItemLine = ({
 	company,
 	companySlug,
+	description,
 	itemKey,
 	projectLink,
-}: ProjectItemLineProps) => (
-	<div className="line-item">
-		<ProductImageWithPopup
-			companySlug={companySlug}
-			itemKey={itemKey}
-			projectLink={projectLink}
-		/>
-		<div className="product-blurb">
-			<p>{company}</p>
+	subtitle,
+}: ProjectItemLineProps) => {
+	const desc = firstItem(description);
+
+	return (
+		<div className="line-item">
+			<ProductImageWithPopup
+				companySlug={companySlug}
+				itemKey={itemKey}
+				projectLink={projectLink}
+			/>
+			<div className="product-blurb">
+				<p className="company">{company}</p>
+				<p className="subtitle">{subtitle}</p>
+				<p className="description truncate">{desc}</p>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 type ProjectItemLineProps = {
 	projectLink: string;
-} & Pick<ProjectType, 'company' | 'companySlug' | 'itemKey'>;
+} & Pick<
+	ProjectType,
+	'company' | 'companySlug' | 'description' | 'itemKey' | 'subtitle'
+>;
