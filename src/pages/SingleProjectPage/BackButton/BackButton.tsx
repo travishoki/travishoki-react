88c6d44b392bc1986d949b569713d14f186191b5
Travis Hoki <travis.hoki@gmail.com>
2026-06-07 03:28:37 -0600

    view all projects button

--- src/pages/SingleProjectPage/BackButton/BackButton.tsx
+++ src/pages/SingleProjectPage/BackButton/BackButton.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const BackButton = () => (
 	<div className="mt-3">
 		<Link className="btn btn-primary" to="/projects">
-			View All
+			View All Projects
 		</Link>
 	</div>
 );
