7ddf1efc7cacaab510ff87be46591ad8f61f7c33
Travis Hoki <travis.hoki@gmail.com>
2026-06-07 23:22:19 -0600

    cleanup styling

--- src/pages/HomePage/Skills/ListSection/ListColumn/ListColumn.tsx
+++ src/pages/HomePage/Skills/ListSection/ListColumn/ListColumn.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './ListColumn.scss';
+
 type ListColumnProps = {
 	items: string[];
 };
 
 export const ListColumn = ({ items }: ListColumnProps) => (
-	<ul>
+	<ul className="list-column">
 		{items.map((item, index) => (
 			<li key={index}>{item}</li>
 		))}
--- src/pages/HomePage/Skills/ListSection/ReadMoreButton/ReadMoreButton.tsx
+++ src/pages/HomePage/Skills/ListSection/ReadMoreButton/ReadMoreButton.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './ReadMoreButton.scss';
+
 type ReadMoreButtonProps = {
 	expanded: boolean;
 	onToggle: () => void;
 };
 
 export const ReadMoreButton = ({ expanded, onToggle }: ReadMoreButtonProps) => (
-	<button className="list-section-read-more" onClick={onToggle}>
+	<button className="read-more-button" onClick={onToggle}>
 		<i className={`fa fa-chevron-${expanded ? 'up' : 'down'}`} />
 		{expanded ? 'Read Less' : 'Read More'}
 	</button>
