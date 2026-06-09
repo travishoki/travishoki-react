11410475ab6345232ca4172485cf985558a99a74
Travis Hoki <travis.hoki@gmail.com>
2026-06-09 16:41:09 -0600

    app image gallery

--- src/pages/PassionPage/HokiSkateboards/MobileApp/MobileAppImageGallery/MobileAppImageGallery.tsx
+++ src/pages/PassionPage/HokiSkateboards/MobileApp/MobileAppImageGallery/MobileAppImageGallery.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { CarouselDots } from '~components/CarouselDots/CarouselDots';
+import { ImageModal } from '~components/ImageModal/ImageModal/ImageModal';
 
 import { APP_GALLERY_IMAGES } from './MobileAppImageGallery.data';
 import './MobileAppImageGallery.scss';
@@ -10,13 +11,37 @@ const imageSrc = (filename: string) =>
 
 export const MobileAppImageGallery = () => {
 	const [page, setPage] = useState(0);
+	const [modalOpen, setModalOpen] = useState(false);
+	const [dimensions, setDimensions] = useState<number[]>([0, 0]);
+
+	const goToPage = (index: number) =>
+		setPage((index + APP_GALLERY_IMAGES.length) % APP_GALLERY_IMAGES.length);
+
+	const currentSrc = imageSrc(APP_GALLERY_IMAGES[page]);
 
 	return (
 		<div className="mobile-app-image-gallery">
+			{modalOpen && (
+				<ImageModal
+					dimensions={dimensions}
+					onClose={() => setModalOpen(false)}
+					onNext={() => goToPage(page + 1)}
+					onPrev={() => goToPage(page - 1)}
+					src={currentSrc}
+				/>
+			)}
+
 			<img
 				alt={`Hoki Skateboards app screen ${page + 1}`}
 				className="mobile-app-image-gallery-image"
-				src={imageSrc(APP_GALLERY_IMAGES[page])}
+				onClick={() => setModalOpen(true)}
+				onLoad={(event) =>
+					setDimensions([
+						event.currentTarget.naturalWidth,
+						event.currentTarget.naturalHeight,
+					])
+				}
+				src={currentSrc}
 			/>
 
 			<CarouselDots
