d58cff65d7bd564ca8a1fa6967ccbb396554dec7
Travis Hoki <travis.hoki@gmail.com>
2026-06-09 16:16:07 -0600

    breakout download section

---
+++ src/pages/PassionPage/HokiSkateboards/MobileApp/DownloadAppSection/DownloadAppSection.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { AppBadges } from '../AppBadges';
+import './DownloadAppSection.scss';
+
+export const DownloadAppSection = () => (
+	<div className="download-app-section">
+		<h3>Download My App!</h3>
+
+		<p className="indent">
+			Built in React Native with Typescript, using the react-native-game-engine
+			and MatterJs.
+		</p>
+
+		<p className="indent">
+			Currently in the Apple Store, will be back in the Google Play store soon!
+		</p>
+
+		<AppBadges />
+
+		<p className="copyright">
+			Apple and the Apple logo are trademarks of Apple Inc. Android, Google Play
+			and the Google Play logo are trademarks of Google LLC.
+		</p>
+	</div>
+);
--- src/pages/PassionPage/HokiSkateboards/MobileApp/MobileApp.tsx
+++ src/pages/PassionPage/HokiSkateboards/MobileApp/MobileApp.tsx
@@ -13,7 +13,7 @@ import {
 
 import { PassionLabel } from '../PassionLabel/PassionLabel';
 import { Title } from '../Title/Title';
-import { AppBadges } from './AppBadges';
+import { DownloadAppSection } from './DownloadAppSection/DownloadAppSection';
 import { MobileAppImageGallery } from './MobileAppImageGallery/MobileAppImageGallery';
 import './MobileApp.scss';
 
@@ -48,25 +48,6 @@ export const MobileApp = () => (
 			</div>
 		</div>
 
-		<div className="download-app-section">
-			<h3>Download My App!</h3>
-
-			<p className="indent">
-				Built in React Native with Typescript, using the
-				react-native-game-engine and MatterJs.
-			</p>
-
-			<p className="indent">
-				Currently in the Apple Store, will be back in the Google Play store
-				soon!
-			</p>
-
-			<AppBadges />
-
-			<p className="copyright">
-				Apple and the Apple logo are trademarks of Apple Inc. Android, Google
-				Play and the Google Play logo are trademarks of Google LLC.
-			</p>
-		</div>
+		<DownloadAppSection />
 	</div>
 );
