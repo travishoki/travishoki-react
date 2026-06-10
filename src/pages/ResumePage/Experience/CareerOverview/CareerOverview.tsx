9f43780703830a5f55b1c228b615e5b213c25d33
Travis Hoki <travis.hoki@gmail.com>
2026-06-10 10:25:05 -0600

    career overview

--- src/pages/ResumePage/Experience/CareerOverview/CareerOverview.tsx
+++ src/pages/ResumePage/Experience/CareerOverview/CareerOverview.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export const CareerOverview = () => {
-	return <h2>Career Overview</h2>;
+	return (
+		<Fragment>
+			<h2>Career Overview</h2>
+			<p className="indent">
+				Highly accomplished Senior Frontend Engineer with over 14 years of
+				experience building, scaling, and optimizing enterprise-grade web
+				applications. Proven track record of architecting robust
+				internationalization infrastructure, automating engineering code
+				cleanliness, and leading high-leverage framework migrations while
+				establishing standards and mentoring technical teams.
+			</p>
+		</Fragment>
+	);
 };
