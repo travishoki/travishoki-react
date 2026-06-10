0fbadf8722cee135c90bb0c9e41bcfe9597bf648
Travis Hoki <travis.hoki@gmail.com>
2026-06-10 12:41:22 -0600

    scripts log title

--- scripts/stats.mjs
+++ scripts/stats.mjs
@@ -1,5 +1,15 @@
+import { log } from 'console';
+
+import chalk from 'chalk';
+
 import { logDeadCodeStats } from './dead-code.mjs';
 import { logLintWarningStats } from './lint.mjs';
+import { logSpacer } from './logging.mjs';
+
+// -- Run --
+
+log(chalk.bold.underline('========== Stats =========='));
+logSpacer();
 
 logDeadCodeStats();
 logLintWarningStats();
