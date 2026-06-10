35b60873009fbae8abb61af6af34b84f3da158e9
Travis Hoki <travis.hoki@gmail.com>
2026-06-10 10:42:39 -0600

    breakout how its made links

--- src/pages/HomePage/HowItsMade/HowItsMade.tsx
+++ src/pages/HomePage/HowItsMade/HowItsMade.tsx
@@ -1,39 +1,15 @@
 import React from 'react';
 
-import './HowItsMade.scss';
+import { HowItsMadeLinks } from './HowItsMadeLinks/HowItsMadeLinks';
 import { HowItsMadeList } from './HowItsMadeList/HowItsMadeList';
+import './HowItsMade.scss';
 
 export const HowItsMade = () => {
 	return (
 		<section className="how-its-made">
 			<h2>How This Site Was Made</h2>
 
-			<div className="how-its-made-content">
-				<div>
-					<a
-						className="how-its-made-link"
-						href="https://github.com/travishoki/travishoki-react"
-						rel="noreferrer"
-						target="_blank"
-					>
-						<i className="fa fa-github" />
-						<p className="how-its-made-link-label">Github</p>
-						<p className="how-its-made-link-description">Source Code</p>
-					</a>
-				</div>
-				<div>
-					<a
-						className="how-its-made-link"
-						href="https://travishoki.com/storybook-static"
-						rel="noreferrer"
-						target="_blank"
-					>
-						<i className="fa fa-book" />
-						<p className="how-its-made-link-label">Storybook</p>
-						<p className="how-its-made-link-description">Component Library</p>
-					</a>
-				</div>
-			</div>
+			<HowItsMadeLinks />
 			<HowItsMadeList />
 		</section>
 	);
---
+++ src/pages/HomePage/HowItsMade/HowItsMadeLinks/HowItsMadeLinks.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './HowItsMadeLinks.scss';
+
+export const HowItsMadeLinks = () => {
+	return (
+		<div className="how-its-made-links">
+			<div>
+				<a
+					className="how-its-made-link"
+					href="https://github.com/travishoki/travishoki-react"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i className="fa fa-github" />
+					<p className="how-its-made-link-label">Github</p>
+					<p className="how-its-made-link-description">Source Code</p>
+				</a>
+			</div>
+			<div>
+				<a
+					className="how-its-made-link"
+					href="https://travishoki.com/storybook-static"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i className="fa fa-book" />
+					<p className="how-its-made-link-label">Storybook</p>
+					<p className="how-its-made-link-description">Component Library</p>
+				</a>
+			</div>
+		</div>
+	);
+};
