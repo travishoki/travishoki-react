7500b6de6ac430862ef245e7ca831a0dcc066573
Travis Hoki <travis.hoki@gmail.com>
2026-06-10 13:03:27 -0600

    sync the colors in js and css

--- src/const/colors.const.ts
+++ src/const/colors.const.ts
@@ -1,6 +1,13 @@
-// Colors
+// Black & White
 export const WHITE = '#ffffff';
 export const BLACK = '#000000';
+
+// Gray
+export const GRAY_1 = '#BBBBBB';
+export const GRAY_2 = '#777777';
+export const GRAY_3 = '#555555';
+export const GRAY_4 = '#404040';
+export const GRAY_5 = '#373737';
 
 // Blue
 export const BLUE_1 = '#7c8fce';
@@ -9,13 +16,6 @@ export const BLUE_3 = '#415082';
 export const BLUE_4 = '#27314f';
 export const BLUE_5 = '#1b2136';
 
-// Red
-export const RED_1 = '#b48d96';
-export const RED_2 = '#9b6672';
-export const RED_3 = '#824150';
-export const RED_4 = '#683440';
-export const RED_5 = '#412028';
-
 // Green
 export const GREEN_1 = '#96b48d';
 export const GREEN_2 = '#729b66';
@@ -23,12 +23,12 @@ export const GREEN_3 = '#508241';
 export const GREEN_4 = '#406834';
 export const GREEN_5 = '#284120';
 
-// Gray
-export const GRAY_1 = '#BBBBBB';
-export const GRAY_2 = '#777777';
-export const GRAY_3 = '#555555';
-export const GRAY_4 = '#404040';
-export const GRAY_5 = '#373737';
+// Red
+export const RED_1 = '#b48d96';
+export const RED_2 = '#9b6672';
+export const RED_3 = '#824150';
+export const RED_4 = '#683440';
+export const RED_5 = '#412028';
 
 // Primary
 export const PRIMARY_1 = BLUE_1;
