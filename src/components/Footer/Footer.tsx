186023b9aac2db91a6548db0fc07e728de34a267
Travis Hoki <travis.hoki@gmail.com>
2026-06-11 17:10:51 -0600

    css modules footer and subfooter

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import { BackToTop } from './BackToTop/BackToTop';
 import { SubFooter } from './SubFooter/SubFooter';
 
-import './Footer.scss';
+import styles from './Footer.module.scss';
 
 const YEAR = new Date().getFullYear();
 export const Footer = () => (
 	<>
 		<SubFooter />
-		<div className="center" id="footer">
+		<div className={classNames('center', styles.footer)}>
 			<p>©{YEAR} Travishoki.com All Rights Reserved</p>
 			<BackToTop />
 		</div>
--- src/components/Footer/SubFooter/SubFooter.tsx
+++ src/components/Footer/SubFooter/SubFooter.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 
 import { BluehostAffiliate } from './BluehostAffiliate/BluehostAffiliate';
 import { SocialIcons } from '../../SocialIcons/SocialIcons';
 
-import './SubFooter.scss';
+import styles from './SubFooter.module.scss';
 
 export const SubFooter = () => (
-	<div id="subfooter">
-		<div className="contact-info m-2">
-			<SocialIcons className="subfooter-social" />
-			<NavLink className="active contact-link ms-3" to="/contact">
+	<div className={styles.subfooter}>
+		<div className={classNames(styles['contact-info'], 'm-2')}>
+			<SocialIcons className={styles['subfooter-social']} />
+			<NavLink
+				className={classNames(styles['contact-link'], 'active ms-3')}
+				to="/contact"
+			>
 				Contact
 			</NavLink>
 		</div>
