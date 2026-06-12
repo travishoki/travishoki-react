1f2362ded79134d50cdd0c088aa845fc3a92a87d
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 03:09:50 -0600

    fix test

--- src/pages/ProjectsPage/Projects/Projects.helpers.test.ts
+++ src/pages/ProjectsPage/Projects/Projects.helpers.test.ts
@@ -3,15 +3,17 @@ import expect from 'expect';
 
 import { getResultsCountClass } from './Projects.helpers';
 
+import styles from './Projects.module.scss';
+
 describe('getResultsCountClass', () => {
-	it('returns results-<n> when below the max column count', () => {
-		expect(getResultsCountClass(1)).toBe('results1');
-		expect(getResultsCountClass(2)).toBe('results2');
-		expect(getResultsCountClass(3)).toBe('results3');
+	it('returns results<n> when below the max column count', () => {
+		expect(getResultsCountClass(1)).toBe(styles.results1);
+		expect(getResultsCountClass(2)).toBe(styles.results2);
+		expect(getResultsCountClass(3)).toBe(styles.results3);
 	});
 
 	it('returns results-full at or above the max column count', () => {
-		expect(getResultsCountClass(4)).toBe('resultsFull');
-		expect(getResultsCountClass(10)).toBe('resultsFull');
+		expect(getResultsCountClass(4)).toBe(styles.resultsFull);
+		expect(getResultsCountClass(10)).toBe(styles.resultsFull);
 	});
 });
