5df4c3541f511ca60bd11e9216edffc3971dd0ac
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 01:40:31 -0600

    fix styling

--- src/pages/PassionPage/HokiSkateboards/Content/ProjectImage/ProjectImage.tsx
+++ src/pages/PassionPage/HokiSkateboards/Content/ProjectImage/ProjectImage.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import classNames from 'classnames';
-
 import style from './ProjectImage.module.scss';
 
 export const ProjectImage = ({ alt, src }: ProjectImageProps) => {
-	return (
-		<img
-			alt={alt}
-			className={classNames('center', style['project-image'])}
-			src={src}
-		/>
-	);
+	return <img alt={alt} className={style.projectImage} src={src} />;
 };
 
 type ProjectImageProps = {
