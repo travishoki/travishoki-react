406e5bb7bb4a50e4f746a224dd400c4bb9fe0817
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 01:47:05 -0600

    fix styling

--- src/pages/SingleProjectPage/SingleProjectPage.tsx
+++ src/pages/SingleProjectPage/SingleProjectPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import classNames from 'classnames';
 import { useParams } from 'react-router-dom';
 
 import { ProjectContent } from './ProjectContent/ProjectContent';
 import { PROJECTS_DATA } from '../ProjectsPage/ProjectsPage.data';
 import { BackButton } from './BackButton/BackButton';
+
+import styles from './SingleProjectPage.module.scss';
 
 export const SingleProjectPage = () => {
 	const { paramItemKey } = useParams();
@@ -16,7 +19,7 @@ export const SingleProjectPage = () => {
 
 	return (
 		<div className="projects" id="container">
-			<div className="projectsContainer grid">
+			<div className={classNames(styles.singleProjectsContainer, 'grid')}>
 				<BackButton />
 				<ProjectContent project={singleProject} />
 			</div>
