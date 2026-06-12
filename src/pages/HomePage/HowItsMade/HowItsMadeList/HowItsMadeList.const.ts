a6f6466c76107b980d921f2a854a05161ef375e7
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 10:14:11 -0600

    how its made links

--- src/pages/HomePage/HowItsMade/HowItsMadeList/HowItsMadeList.const.ts
+++ src/pages/HomePage/HowItsMade/HowItsMadeList/HowItsMadeList.const.ts
@@ -1,12 +1,36 @@
 import { ReadMoreListItem } from '~components/ReadMoreList/ReadMoreList.types';
 
 export const ITEMS: ReadMoreListItem[] = [
-	{ text: 'Typed with Typescript' },
-	{ text: 'Unit testing with Jest' },
-	{ text: 'Eslint for consistent coding style' },
-	{ text: 'Storybook for component management' },
-	{ text: 'Husky automatically runs scripts' },
-	{ text: 'Build with some assistance from Claude Code' },
+	{
+		link: 'https://www.typescriptlang.org/',
+		linkTitle: 'Typescript NPM Module',
+		text: 'Typed with Typescript',
+	},
+	{
+		link: 'https://jestjs.io/docs/getting-started',
+		linkTitle: 'Jest',
+		text: 'Unit testing with Jest',
+	},
+	{
+		link: 'https://eslint.org/',
+		linkTitle: 'Eslint',
+		text: 'Eslint for consistent coding style',
+	},
+	{
+		link: 'https://storybook.js.org/',
+		linkTitle: 'Storybook',
+		text: 'Storybook for component management',
+	},
+	{
+		link: 'https://typicode.github.io/husky/',
+		linkTitle: 'Husky',
+		text: 'Husky automatically runs scripts',
+	},
+	{
+		link: 'https://claude.com/',
+		linkTitle: 'Claude Code',
+		text: 'Build with some assistance from Claude Code',
+	},
 	{
 		link: 'https://chromewebstore.google.com/detail/axe-devtools-web-accessib/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US',
 		linkTitle: 'axe DevTools Google Chrome Plugin',
