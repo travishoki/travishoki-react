d4e8c683aa6c9d07b477dceaddafe4a83b881ac6
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 00:33:27 -0600

    result styling

--- src/pages/ProjectsPage/Projects/Projects.helpers.ts
+++ src/pages/ProjectsPage/Projects/Projects.helpers.ts
@@ -1,9 +1,11 @@
+import styles from './Projects.module.scss';
+
 export const getResultsCountClass = (num: number) => {
 	const MAX_COLUMN_COUNT = 4;
 
 	if (num < MAX_COLUMN_COUNT) {
-		return `results-${num}`;
+		return styles[`results${num}`];
 	}
 
-	return 'results-full';
+	return styles.resultsFull;
 };
