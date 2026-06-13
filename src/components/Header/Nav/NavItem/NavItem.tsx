cb3b4ce6660136b182c0ca0399c5fc32aac69d1b
Travis Hoki <travis.hoki@gmail.com>
2026-06-12 19:07:25 -0600

    nav active class

--- src/components/Header/Nav/NavItem/NavItem.tsx
+++ src/components/Header/Nav/NavItem/NavItem.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
+import styles from './NavItem.module.scss';
+
 export const NavItem = ({ title, to }: NavItemProps) => {
 	return (
 		<li>
 			<NavLink
-				className={(navData) => (navData.isActive ? 'active' : '')}
+				className={(navData) => (navData.isActive ? styles.active : '')}
 				to={to}
 			>
 				{title}
