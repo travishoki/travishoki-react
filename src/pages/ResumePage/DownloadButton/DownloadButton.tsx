79ab544dfa61170f05b0a2d055a05f8bd2a44a9c
Travis Hoki <travis.hoki@gmail.com>
2026-06-15 02:12:39 -0600

    download buttons

--- src/pages/ResumePage/DownloadButton/DownloadButton.tsx
+++ src/pages/ResumePage/DownloadButton/DownloadButton.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import styles from './DownloadButton.module.scss';
 
-export const DownloadButton = () => (
-	<a
-		className={`btn btn-secondary btn-lg ${styles.downloadButton}`}
-		href="http://travishoki.com/travis-hoki-resume.pdf"
-		rel="noreferrer"
-		target="_blank"
-	>
-		<i className="fa fa-download" /> Download
-	</a>
+export const DownloadButton = ({
+	caption,
+	href,
+	icon,
+	text,
+	title,
+}: DownloadButtonProps) => (
+	<div>
+		<a
+			className={`btn btn-secondary btn-lg ${styles.downloadButton}`}
+			href={href}
+			rel="noreferrer"
+			target="_blank"
+			title={title}
+		>
+			{text} <i className={classNames('fa', icon)} />
+		</a>
+		{caption && <p className={styles.caption}>({caption})</p>}
+	</div>
 );
+
+type DownloadButtonProps = {
+	caption?: string;
+	href: string;
+	icon: string;
+	text: string;
+	title: string;
+};
--- src/pages/ResumePage/DownloadSection/DownloadSection.tsx
+++ src/pages/ResumePage/DownloadSection/DownloadSection.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import { DownloadButton } from '../DownloadButton/DownloadButton';
 
+import styles from './DownloadSection.module.scss';
+
 export const DownloadSection = () => (
-	<section className="blue">
-		<DownloadButton />
-		<p className="center m-0">Open a link to the PDF Version</p>
+	<section className="blue ">
+		<div className={classNames('boxed', styles.resumeButtons)}>
+			<DownloadButton
+				caption="The one I have to use"
+				href="/travis-hoki-resume.pdf"
+				icon="fa-link"
+				text=".pdf"
+				title="Travis Hoki Resume (pdf)"
+			/>
+			<DownloadButton
+				caption="The one I like"
+				href="/travis-hoki-resume-creative.pdf"
+				icon="fa-link"
+				text="Creative .pdf"
+				title="Travis Hoki Resume Creative (pdf)"
+			/>
+			<DownloadButton
+				caption="The one that may be required"
+				href="/travis-hoki-resume.docx"
+				icon="fa-download"
+				text=".docx"
+				title="Travis Hoki Resume (docx)"
+			/>
+		</div>
 	</section>
 );
