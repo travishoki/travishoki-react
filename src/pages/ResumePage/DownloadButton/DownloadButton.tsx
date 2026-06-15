e26d20adc04b6738d3ddd87b68c7145378edeaef
Travis Hoki <travis.hoki@gmail.com>
2026-06-15 02:46:50 -0600

    resume download button restructure

--- src/pages/ResumePage/DownloadButton/DownloadButton.tsx
+++ src/pages/ResumePage/DownloadButton/DownloadButton.tsx
@@ -5,27 +5,22 @@ import classNames from 'classnames';
 import styles from './DownloadButton.module.scss';
 
 export const DownloadButton = ({
-	caption,
 	icon,
 	text,
 	...rest
 }: DownloadButtonProps) => (
-	<div className={styles.buttonContainer}>
-		{caption && <p className={styles.caption}>({caption})</p>}
-		<a
-			className={styles.downloadButton}
-			rel="noreferrer"
-			target="_blank"
-			{...rest}
-		>
-			<div className={styles.buttonText}>{text}</div>{' '}
-			<i className={classNames('fa', icon, styles.icon)} />
-		</a>
-	</div>
+	<a
+		className={styles.downloadButton}
+		rel="noreferrer"
+		target="_blank"
+		{...rest}
+	>
+		<div className={styles.buttonText}>{text}</div>{' '}
+		<i className={classNames('fa', icon, styles.icon)} />
+	</a>
 );
 
 type DownloadButtonProps = {
-	caption?: string;
 	href: string;
 	icon: string;
 	text: string;
--- src/pages/ResumePage/DownloadSection/DownloadSection.tsx
+++ src/pages/ResumePage/DownloadSection/DownloadSection.tsx
@@ -9,27 +9,33 @@ import styles from './DownloadSection.module.scss';
 export const DownloadSection = () => (
 	<section className="blue ">
 		<div className={classNames('boxed', styles.resumeButtons)}>
-			<DownloadButton
-				caption="The one I have to use"
-				href="/travis-hoki-resume.pdf"
-				icon="fa-link"
-				text=".pdf"
-				title="Travis Hoki Resume (pdf)"
-			/>
-			<DownloadButton
-				caption="The one I like"
-				href="/travis-hoki-resume-creative.pdf"
-				icon="fa-link"
-				text="Creative .pdf"
-				title="Travis Hoki Resume Creative (pdf)"
-			/>
-			<DownloadButton
-				caption="The one that may be required"
-				href="/travis-hoki-resume.docx"
-				icon="fa-download"
-				text=".docx"
-				title="Travis Hoki Resume (docx)"
-			/>
+			<div>
+				<p className={styles.caption}>(The one I have to use)</p>
+				<DownloadButton
+					href="/travis-hoki-resume.pdf"
+					icon="fa-link"
+					text=".pdf"
+					title="Travis Hoki Resume (pdf)"
+				/>
+			</div>
+			<div>
+				<p className={styles.caption}>(The one I like)</p>
+				<DownloadButton
+					href="/travis-hoki-resume-creative.pdf"
+					icon="fa-link"
+					text="Creative .pdf"
+					title="Travis Hoki Resume Creative (pdf)"
+				/>
+			</div>
+			<div>
+				<p className={styles.caption}>(The one that may be required)</p>
+				<DownloadButton
+					href="/travis-hoki-resume.docx"
+					icon="fa-download"
+					text=".docx"
+					title="Travis Hoki Resume (docx)"
+				/>
+			</div>
 		</div>
 	</section>
 );
