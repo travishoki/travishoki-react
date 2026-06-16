c677c530149efeff09585d89b1b0c0b048827140
Travis Hoki <travis.hoki@gmail.com>
2026-06-16 02:56:59 -0600

    cleanup

--- src/helpers/numbers.ts
+++ src/helpers/numbers.ts
@@ -1,8 +1,2 @@
 // eslint-disable-next-line no-magic-numbers
 export const bisect = (num: number) => num / 2;
-
-export const cap = (num: number, max: number) => {
-	if (num > max) return max;
-
-	return num;
-};
