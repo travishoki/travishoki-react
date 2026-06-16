d0e6ec621044bdad02767c39d64fa31eb9a37f47
Travis Hoki <travis.hoki@gmail.com>
2026-06-16 03:06:25 -0600

    animate contact page image

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { motion } from 'motion/react';
+
 import ImgProfilePic from '~images/global/travis-hoki-2.jpg';
 
 import { ContactInfo } from './ContactInfo/ContactInfo';
@@ -10,11 +12,13 @@ export const ContactPage = () => (
 			<div className="pt-4">
 				<div className="row">
 					<div className="col-sm-6">
-						<img
-							alt="Travis Hoki Profile Picture"
-							className="pb-3"
-							src={ImgProfilePic}
-						/>
+						<motion.div animate={{ scale: 1 }} initial={{ scale: 0.5 }}>
+							<img
+								alt="Travis Hoki Profile Picture"
+								className="pb-3"
+								src={ImgProfilePic}
+							/>
+						</motion.div>
 					</div>
 
 					<div className="col-sm-6">
