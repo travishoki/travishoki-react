feaa32f65d209413b1776ffa68624c8d55a94dda
Travis Hoki <travis.hoki@gmail.com>
2026-06-17 14:33:50 -0600

    hoki skateboards breakout cover image

---
+++ src/pages/PassionPage/HokiSkateboards/CoverImage/CoverImage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ImageMaximizable } from '~components/ImageModal/ImageMaximizable/ImageMaximizable';
+import ImgHokiSkateboardsSmall from '~images/passion/hoki-skateboards/hoki-skateboards-small.jpg';
+import ImgHokiSkateboards from '~images/passion/hoki-skateboards/hoki-skateboards.jpg';
+
+import styles from './CoverImage.module.scss';
+
+const SMALL_WIDTH = 600;
+const SMALL_HEIGHT = 400;
+
+const LARGE_WIDTH = 1541;
+const LARGE_HEIGHT = 1030;
+
+export const CoverImage = () => (
+	<ImageMaximizable
+		alt="Hoki Skateboards"
+		className={styles.mainImage}
+		dimensions={[SMALL_WIDTH, SMALL_HEIGHT]}
+		src={ImgHokiSkateboardsSmall}
+		srcLarge={ImgHokiSkateboards}
+		srcLargeDimensions={[LARGE_WIDTH, LARGE_HEIGHT]}
+	/>
+);
--- src/pages/PassionPage/HokiSkateboards/HokiSkateboards.tsx
+++ src/pages/PassionPage/HokiSkateboards/HokiSkateboards.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
 
-import { ImageMaximizable } from '~components/ImageModal/ImageMaximizable/ImageMaximizable';
-import ImgHokiSkateboardsSmall from '~images/passion/hoki-skateboards/hoki-skateboards-small.jpg';
-import ImgHokiSkateboards from '~images/passion/hoki-skateboards/hoki-skateboards.jpg';
-
 import { Content } from './Content/Content';
-import { Logo } from './Logo/Logo';
+import { CoverImage } from './CoverImage/CoverImage';
 import { MobileApp } from './MobileApp/MobileApp';
 import { SocialMedia } from './SocialMedia/SocialMedia';
 
 import styles from './HokiSkateboards.module.scss';
 
-const SMALL_WIDTH = 600;
-const SMALL_HEIGHT = 400;
-
-const LARGE_WIDTH = 1541;
-const LARGE_HEIGHT = 1030;
-
 export const HokiSkateboards = () => (
 	<section className="boxed boxed-lg p-3">
-		<Logo />
-		<ImageMaximizable
-			alt="Hoki Skateboards"
-			className={styles.mainImage}
-			dimensions={[SMALL_WIDTH, SMALL_HEIGHT]}
-			src={ImgHokiSkateboardsSmall}
-			srcLarge={ImgHokiSkateboards}
-			srcLargeDimensions={[LARGE_WIDTH, LARGE_HEIGHT]}
-		/>
+		<CoverImage />
 		<p className={styles.passionProjectDescription}>
 			Hoki Skateboards is my passion project. Through the years I have enjoyed
 			building to my little company. I&apos;ve made and sold skateboards, wrote
