0a7e81187e862446f0a707da5b9c59838dbcd8ea
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 02:54:21 -0600

    React.lazy Suspense

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,43 +1,85 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { App } from './App';
-import { AboutPage } from './pages/AboutPage/AboutPage';
-import { ContactPage } from './pages/ContactPage/ContactPage';
-import { EducationPage } from './pages/EducationPage/EducationPage';
-import { HomePage } from './pages/HomePage/HomePage';
-import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
-import { FightForYourDreamsPage } from './pages/PassionPage/FightForYourDreamsPage/FightForYourDreamsPage';
-import { HokiSkateboardsPage } from './pages/PassionPage/HokiSkateboardsPage/HokiSkateboardsPage';
-import { ProjectsPage } from './pages/ProjectsPage/ProjectsPage';
-import { ResumePage } from './pages/ResumePage/ResumePage';
-import { SingleProjectPage } from './pages/SingleProjectPage/SingleProjectPage';
+
+const AboutPage = React.lazy(() =>
+	import('./pages/AboutPage/AboutPage').then((m) => ({ default: m.AboutPage })),
+);
+const ContactPage = React.lazy(() =>
+	import('./pages/ContactPage/ContactPage').then((m) => ({
+		default: m.ContactPage,
+	})),
+);
+const EducationPage = React.lazy(() =>
+	import('./pages/EducationPage/EducationPage').then((m) => ({
+		default: m.EducationPage,
+	})),
+);
+const FightForYourDreamsPage = React.lazy(() =>
+	import('./pages/PassionPage/FightForYourDreamsPage/FightForYourDreamsPage').then(
+		(m) => ({ default: m.FightForYourDreamsPage }),
+	),
+);
+const HokiSkateboardsPage = React.lazy(() =>
+	import('./pages/PassionPage/HokiSkateboardsPage/HokiSkateboardsPage').then(
+		(m) => ({ default: m.HokiSkateboardsPage }),
+	),
+);
+const HomePage = React.lazy(() =>
+	import('./pages/HomePage/HomePage').then((m) => ({ default: m.HomePage })),
+);
+const NotFoundPage = React.lazy(() =>
+	import('./pages/NotFoundPage/NotFoundPage').then((m) => ({
+		default: m.NotFoundPage,
+	})),
+);
+const ProjectsPage = React.lazy(() =>
+	import('./pages/ProjectsPage/ProjectsPage').then((m) => ({
+		default: m.ProjectsPage,
+	})),
+);
+const ResumePage = React.lazy(() =>
+	import('./pages/ResumePage/ResumePage').then((m) => ({
+		default: m.ResumePage,
+	})),
+);
+const SingleProjectPage = React.lazy(() =>
+	import('./pages/SingleProjectPage/SingleProjectPage').then((m) => ({
+		default: m.SingleProjectPage,
+	})),
+);
 
 export const AppRoutes = () => (
 	<Router>
 		<App>
-			<Routes>
-				<Route element={<HomePage />} path="/" />
-				<Route
-					element={<ProjectsPage />}
-					path="/projects/:paramFilter?/:paramSearch?"
-				/>
-				<Route element={<SingleProjectPage />} path="/project/:paramItemKey" />
-				<Route element={<ResumePage />} path="/resume" />
-				<Route element={<EducationPage />} path="/education" />
-				<Route element={<ContactPage />} path="/contact" />
-				<Route element={<AboutPage />} path="/about" />
-				<Route
-					element={<HokiSkateboardsPage />}
-					path="/passion/hoki-skateboards"
-				/>
-				<Route
-					element={<FightForYourDreamsPage />}
-					path="/passion/fight-for-your-dreams"
-				/>
-				<Route element={<NotFoundPage />} path="*" />
-			</Routes>
+			<Suspense fallback={null}>
+				<Routes>
+					<Route element={<HomePage />} path="/" />
+					<Route
+						element={<ProjectsPage />}
+						path="/projects/:paramFilter?/:paramSearch?"
+					/>
+					<Route
+						element={<SingleProjectPage />}
+						path="/project/:paramItemKey"
+					/>
+					<Route element={<ResumePage />} path="/resume" />
+					<Route element={<EducationPage />} path="/education" />
+					<Route element={<ContactPage />} path="/contact" />
+					<Route element={<AboutPage />} path="/about" />
+					<Route
+						element={<HokiSkateboardsPage />}
+						path="/passion/hoki-skateboards"
+					/>
+					<Route
+						element={<FightForYourDreamsPage />}
+						path="/passion/fight-for-your-dreams"
+					/>
+					<Route element={<NotFoundPage />} path="*" />
+				</Routes>
+			</Suspense>
 		</App>
 	</Router>
 );
