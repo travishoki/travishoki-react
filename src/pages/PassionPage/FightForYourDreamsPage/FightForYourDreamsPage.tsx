b60c53b5e5ea0848c33c2f04b9fbb2d93ce53031
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 02:08:06 -0600

    ffyd

--- src/pages/PassionPage/FightForYourDreamsPage/FightForYourDreamsPage.tsx
+++ src/pages/PassionPage/FightForYourDreamsPage/FightForYourDreamsPage.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-import { FightForYourDreams } from './FightForYourDreams';
+import { ImageGallery } from '~components/ImageGallery/ImageGallery';
+
+import { ButtonLink } from '../ButtonLink/ButtonLink';
 import { PassionNav } from '../PassionNav/PassionNav';
+import { About } from './About/About';
+import { BookHeading } from './BookHeading/BookHeading';
+import { CoverImage } from './CoverImage/CoverImage';
+import { Discription } from './Description/Description';
+import { AMAZON_HREF, GALLERY_IMAGES } from './FightForYourDreams.const';
 
 export const FightForYourDreamsPage = () => (
 	<div id="container">
 		<PassionNav />
-		<FightForYourDreams />
+		<section className="boxed boxed-lg p-3">
+			<BookHeading />
+			<CoverImage />
+			<div className="row">
+				<div className="col-sm-6 pt-3">
+					<div className="boxed boxed-lg">
+						<About />
+						<ButtonLink href={AMAZON_HREF} text="View on Amazon" />
+					</div>
+				</div>
+				<div className="col-sm-6 pt-3">
+					<ImageGallery images={GALLERY_IMAGES} />
+					<Discription />
+				</div>
+			</div>
+		</section>
 	</div>
 );
