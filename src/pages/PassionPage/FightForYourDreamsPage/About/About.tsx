f7078713c13ef6403f9019cacecab251b436a618
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 02:09:11 -0600

    ffyd indent

--- src/pages/PassionPage/FightForYourDreamsPage/About/About.tsx
+++ src/pages/PassionPage/FightForYourDreamsPage/About/About.tsx
@@ -3,27 +3,27 @@ import React from 'react';
 export const About = () => {
 	return (
 		<>
-			<p>
+			<p className="indent">
 				Kaytlin was my wife&apos;s roommate in college and her bridesmaid at our
 				wedding. She is now a professional MMA fighter! Over the last 11 years
 				Lindsey and I have been to many of her fights in person, or watched her
 				fights on TV.
 			</p>
-			<p>
+			<p className="indent">
 				This was my pandemic project. Kaytlin talked to me about an idea she had
 				for a book and I started initial sketches 3 years ago!
 			</p>
-			<p>
+			<p className="indent">
 				I&apos;ve had a lot of help and critiques from my wife, niece, family,
 				and friends. Big thanks to everyone that helped out!
 			</p>
-			<p>
+			<p className="indent">
 				The book launch event will be at Beyond Sports TaeKwonDo studio, the
 				same studio I practiced at 20 years ago! Still ran by the Sorenson
 				family. The book is sponsored by Kaytlin&apos;s athletic sponsor,
 				Century Martial Arts.
 			</p>
-			<p>
+			<p className="indent">
 				Come help us celebrate! Kaytlin is offering a free Kickboxing lesson and
 				book reading. Books will be available at the event, Kaytlin and I will
 				sign them. It will be fun!
