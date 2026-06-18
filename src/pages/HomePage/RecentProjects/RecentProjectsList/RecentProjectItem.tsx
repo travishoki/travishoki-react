8febd96cd5dff8eb37584aac15947c8d76b3e33b
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 14:47:42 -0600

    home page recent project animate in

--- src/pages/HomePage/RecentProjects/RecentProjectsList/RecentProjectItem.tsx
+++ src/pages/HomePage/RecentProjects/RecentProjectsList/RecentProjectItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { ImgPopIn } from '~animations/ImgPopIn';
 import { getImgAltText } from '~helpers/images';
 import {
 	handleThumbnailError,
@@ -13,15 +14,21 @@ import { ProjectType } from '../../../ProjectsPage/ProjectsPage.types';
 import styles from './RecentProjectItem.module.scss';
 
 type RecentProjectItemProps = {
+	index: number;
 	project: ProjectType;
 };
 
-export const RecentProjectItem = ({ project }: RecentProjectItemProps) => {
+const DELAY_MULTIPLIER = 0.2;
+
+export const RecentProjectItem = ({
+	index,
+	project,
+}: RecentProjectItemProps) => {
 	const { company, itemKey, subtitle } = project;
 	const imgAlt = getImgAltText(company, subtitle);
 
 	return (
-		<li key={itemKey} className={styles.recentProjectItem}>
+		<li className={styles.recentProjectItem}>
 			<Link to={`/project/${itemKey}`}>
 				<div className={styles.projectHeader}>
 					<div className={styles.projectHeaderInner}>
@@ -29,11 +36,16 @@ export const RecentProjectItem = ({ project }: RecentProjectItemProps) => {
 						<h4>{subtitle}</h4>
 					</div>
 				</div>
-				<img
+				<ImgPopIn
 					alt={imgAlt}
 					height={250}
 					onError={handleThumbnailError}
 					src={websiteThumbnailSrc(itemKey)}
+					transition={{
+						delay: index * DELAY_MULTIPLIER,
+						duration: 0.4,
+						ease: 'easeInOut',
+					}}
 					width={250}
 				/>
 			</Link>
