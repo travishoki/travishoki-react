8041e487a04e815d756b75e1401c9ae42fcfaef0
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 02:40:45 -0600

    ffyd past tense

--- src/pages/PassionPage/FightForYourDreamsPage/About/About.tsx
+++ src/pages/PassionPage/FightForYourDreamsPage/About/About.tsx
@@ -5,28 +5,29 @@ export const About = () => {
 		<>
 			<p className="indent">
 				Kaytlin was my wife&apos;s roommate in college and her bridesmaid at our
-				wedding. She is now a professional MMA fighter! Over the last 11 years
-				Lindsey and I have been to many of her fights in person, or watched her
-				fights on TV.
+				wedding. She was then a professional MMA fighter! Over the course of 11
+				years Lindsey and I had been to many of her fights in person, or watched
+				her fights on TV.
 			</p>
 			<p className="indent">
-				This was my pandemic project. Kaytlin talked to me about an idea she had
-				for a book and I started initial sketches 3 years ago!
+				This book was my pandemic project. Kaytlin talked to me about an idea
+				she had for a book. It was 3 years from the initial sketches to the book
+				launch.
 			</p>
 			<p className="indent">
-				I&apos;ve had a lot of help and critiques from my wife, niece, family,
-				and friends. Big thanks to everyone that helped out!
+				I had a lot of help and critiques from my wife, niece, family, and
+				friends. Big thanks to everyone that helped out!
 			</p>
 			<p className="indent">
-				The book launch event will be at Beyond Sports TaeKwonDo studio, the
-				same studio I practiced at 20 years ago! Still ran by the Sorenson
-				family. The book is sponsored by Kaytlin&apos;s athletic sponsor,
+				The book launch event was at Beyond Sports TaeKwonDo studio, the same
+				studio I practiced at 20 years ago! It was still run by the Sorenson
+				family. The book was sponsored by Kaytlin&apos;s athletic sponsor,
 				Century Martial Arts.
 			</p>
 			<p className="indent">
-				Come help us celebrate! Kaytlin is offering a free Kickboxing lesson and
-				book reading. Books will be available at the event, Kaytlin and I will
-				sign them. It will be fun!
+				People came to help us celebrate! Kaytlin offered a free Kickboxing
+				lesson and book reading. Books were available at the event, and Kaytlin
+				and I signed them. It was fun!
 			</p>
 		</>
 	);
