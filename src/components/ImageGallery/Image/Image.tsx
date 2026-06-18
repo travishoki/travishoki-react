08eeb8f0d8ac4df614f6886744753d2ffb7efe06
Travis Hoki <travis.hoki@gmail.com>
2026-06-18 01:06:36 -0600

    project img overlay

--- src/components/ImageGallery/Image/Image.tsx
+++ src/components/ImageGallery/Image/Image.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { OverlayWithIcon } from '~components/OverlayWithIcon/OverlayWithIcon';
+
 import { GalleryImage } from '../types';
 
 import styles from './Image.module.scss';
@@ -14,13 +16,10 @@ export const Image = ({ image, setDimensions, setModalOpen }: ImageProps) => {
 		]);
 
 	return (
-		<img
-			alt={alt}
-			className={styles.image}
-			onClick={() => setModalOpen(true)}
-			onLoad={onLoad}
-			src={filename}
-		/>
+		<div className={styles.imageWrapper}>
+			<OverlayWithIcon onClick={() => setModalOpen(true)} />
+			<img alt={alt} className={styles.image} onLoad={onLoad} src={filename} />
+		</div>
 	);
 };
 
