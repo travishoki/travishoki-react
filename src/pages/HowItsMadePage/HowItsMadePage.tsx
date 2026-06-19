f42837ff9a7da1fc8cfa49f5bf381500d790b559
Travis Hoki <travis.hoki@gmail.com>
2026-06-19 17:46:30 -0600

    component return not short hand

--- src/pages/HowItsMadePage/HowItsMadePage.tsx
+++ src/pages/HowItsMadePage/HowItsMadePage.tsx
@@ -9,13 +9,15 @@ import { HowItsMadeList } from './HowItsMadeList/HowItsMadeList';
 
 import styles from './HowItsMadePage.module.scss';
 
-export const HowItsMadePage = () => (
-	<div id="container">
-		<PageSeo seoData={PAGE_SEO[ROUTES.HOW_ITS_MADE]} />
-		<section className={styles.howItsMadePage}>
-			<h2>How This Site Was Made</h2>
-			<HowItsMadeLinks />
-			<HowItsMadeList />
-		</section>
-	</div>
-);
+export const HowItsMadePage = () => {
+	return (
+		<div id="container">
+			<PageSeo seoData={PAGE_SEO[ROUTES.HOW_ITS_MADE]} />
+			<section className={styles.howItsMadePage}>
+				<h2>How This Site Was Made</h2>
+				<HowItsMadeLinks />
+				<HowItsMadeList />
+			</section>
+		</div>
+	);
+};
