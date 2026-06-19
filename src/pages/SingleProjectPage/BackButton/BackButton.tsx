94ba343d8bb59df1b449cecd1a90121e3f56f222
Travis Hoki <travis.hoki@gmail.com>
2026-06-19 15:50:54 -0600

    view all projects with an arrow icon

--- src/pages/SingleProjectPage/BackButton/BackButton.tsx
+++ src/pages/SingleProjectPage/BackButton/BackButton.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const BackButton = () => (
 	<div className="mt-3">
 		<Link className="btn btn-primary" to="/projects">
-			View All Projects
+			<i className="fa fa-chevron-left" /> View All Projects
 		</Link>
 	</div>
 );
