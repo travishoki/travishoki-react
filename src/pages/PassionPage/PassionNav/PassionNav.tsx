5fd30a90fc307cbb71c82056a365d5a5ee26bc79
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 01:15:15 -0600

    passion nav styling

--- src/pages/PassionPage/PassionNav/PassionNav.tsx
+++ src/pages/PassionPage/PassionNav/PassionNav.tsx
@@ -8,34 +8,32 @@ import { ROUTES } from '~const/Routes.const';
 import styles from './PassionNav.module.scss';
 
 export const PassionNav = () => (
-	<section>
-		<div className="boxed">
-			<nav className={styles.passionNav}>
-				<NavLink
-					className={({ isActive }) =>
-						classNames(styles.navButton, { [styles.active]: isActive })
-					}
-					to={ROUTES.HOKI_SKATEBOARDS}
-				>
-					Skate Book <i className="fa fa-book" />
-				</NavLink>
-				<NavLink
-					className={({ isActive }) =>
-						classNames(styles.navButton, { [styles.active]: isActive })
-					}
-					to={ROUTES.MOBILE_APP}
-				>
-					Skate App <i className="fa fa-mobile" />
-				</NavLink>
-				<NavLink
-					className={({ isActive }) =>
-						classNames(styles.navButton, { [styles.active]: isActive })
-					}
-					to={ROUTES.FIGHT_FOR_YOUR_DREAMS}
-				>
-					Fight Book <i className="fa fa-book" />
-				</NavLink>
-			</nav>
-		</div>
-	</section>
+	<div className={classNames('boxed', styles.passionNav)}>
+		<nav>
+			<NavLink
+				className={({ isActive }) =>
+					classNames(styles.navButton, { [styles.active]: isActive })
+				}
+				to={ROUTES.HOKI_SKATEBOARDS}
+			>
+				Skate Book <i className="fa fa-book" />
+			</NavLink>
+			<NavLink
+				className={({ isActive }) =>
+					classNames(styles.navButton, { [styles.active]: isActive })
+				}
+				to={ROUTES.MOBILE_APP}
+			>
+				Skate App <i className="fa fa-mobile" />
+			</NavLink>
+			<NavLink
+				className={({ isActive }) =>
+					classNames(styles.navButton, { [styles.active]: isActive })
+				}
+				to={ROUTES.FIGHT_FOR_YOUR_DREAMS}
+			>
+				Fight Book <i className="fa fa-book" />
+			</NavLink>
+		</nav>
+	</div>
 );
