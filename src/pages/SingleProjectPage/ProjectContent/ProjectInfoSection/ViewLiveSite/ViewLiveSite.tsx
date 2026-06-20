9fc1e2a7420a7e80fbe3016956217c3e79f50e39
Travis Hoki <travis.hoki@gmail.com>
2026-06-19 20:53:46 -0600

    view stie button styling

--- src/pages/SingleProjectPage/ProjectContent/ProjectInfoSection/ViewLiveSite/ViewLiveSite.tsx
+++ src/pages/SingleProjectPage/ProjectContent/ProjectInfoSection/ViewLiveSite/ViewLiveSite.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
+import styles from './ViewLiveSite.module.scss';
+
 export const ViewLiveSite = ({ url }: ViewLiveSiteProps) => (
 	<a
-		className="btn btn-primary btn-lg"
+		className={classNames('btn btn-primary btn-lg', styles.button)}
 		href={url}
 		rel="noreferrer"
 		target="_blank"
