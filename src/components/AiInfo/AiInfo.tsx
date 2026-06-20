211ef56177f6ace2ef6bc527ec99842693c3a9b4
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 01:57:53 -0600

    aiInfo collapsed show one paragraph

--- src/components/AiInfo/AiInfo.tsx
+++ src/components/AiInfo/AiInfo.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import classNames from 'classnames';
 
+import { MotionExpand } from '~animations/MotionExpand';
 import { ParagraphLabel } from '~components/ParagraphLabel/ParagraphLabel';
 import { ParagraphList } from '~components/ParagraphList/ParagraphList';
 import { WHITE } from '~const/colors.const';
@@ -13,7 +14,9 @@ import styles from './AiInfo.module.scss';
 export const AitInfo = ({ text }: AitInfoProps) => {
 	const [expanded, setExpanded] = useState(false);
 
-	const textArray = makeArray(text);
+	const paragraphs = makeArray(text);
+	const [firstParagraph, ...restParagraphs] = paragraphs;
+	const hasMore = restParagraphs.length > 0;
 
 	return (
 		<div className={styles.aiInfo}>
@@ -22,16 +25,21 @@ export const AitInfo = ({ text }: AitInfoProps) => {
 					<SvgIconSparkle color={WHITE} size={22} />
 					<span className={styles.label}>AI Integration</span>
 				</ParagraphLabel>
-				{expanded && <ParagraphList paragraphs={textArray} />}
+				<ParagraphList paragraphs={[firstParagraph]} />
+				<MotionExpand isOpen={expanded}>
+					<ParagraphList paragraphs={restParagraphs} />
+				</MotionExpand>
 			</div>
 
-			<button
-				className={classNames('btn btn-secondary', styles.toggleButton)}
-				onClick={() => setExpanded(!expanded)}
-			>
-				<i className={`fa fa-chevron-${expanded ? 'up' : 'down'}`} />
-				{expanded ? 'Close' : 'Open'}
-			</button>
+			{hasMore && (
+				<button
+					className={classNames('btn btn-secondary', styles.toggleButton)}
+					onClick={() => setExpanded(!expanded)}
+				>
+					<i className={`fa fa-chevron-${expanded ? 'up' : 'down'}`} />
+					{expanded ? 'Close' : 'Open'}
+				</button>
+			)}
 		</div>
 	);
 };
