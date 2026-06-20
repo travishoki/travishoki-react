8755efe48f00b08b0fbe6e52b148ecf45e6fdb07
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 01:31:19 -0600

    feature projects by key

--- src/pages/HomePage/FeaturedProjects/FeaturedProjects.helpers.ts
+++ src/pages/HomePage/FeaturedProjects/FeaturedProjects.helpers.ts
@@ -1,9 +1,15 @@
+import { ProjectItemKey } from 'src/pages/ProjectsPage/ProjectsPage.itemKeys';
 import { ProjectType } from 'src/pages/ProjectsPage/ProjectsPage.types';
 
-export const PODIUM_COUNT = 3;
+export const FEATURED_ITEM_KEYS: ProjectItemKey[] = [
+	'food-cards',
+	'hoki-skateboards-react-native-game',
+	'spiff-translations',
+];
 
 export const orderTopThreeInPodium = (list: ProjectType[]) => {
-	if (list.length < PODIUM_COUNT) return list;
+	/* eslint-disable-next-line no-magic-numbers */
+	if (list.length < 3) return list;
 
 	return [list[1], list[0], list[2]];
 };
