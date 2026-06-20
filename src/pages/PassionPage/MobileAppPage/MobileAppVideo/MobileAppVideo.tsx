b0fdeec96d795b1cd2e71644496f45c508ce1305
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 00:29:40 -0600

    embed mobile game video

--- src/pages/PassionPage/MobileAppPage/MobileAppVideo/MobileAppVideo.tsx
+++ src/pages/PassionPage/MobileAppPage/MobileAppVideo/MobileAppVideo.tsx
@@ -1,29 +1,11 @@
-import React, { useState } from 'react';
-
-import { VideoButton } from '~components/VideoButton/VideoButton';
-import { VideoModal } from '~components/VideoModal/VideoModal';
+import React from 'react';
 
 import styles from './MobileAppVideo.module.scss';
 
-const APP_ICON =
-	'http://www.hokiskateboards.com/wp-content/themes/hokiskateboards-wp/src/images/app/icon.png';
-const VIDEO_SRC = 'videos/game-launch.mp4';
+const VIDEO_SRC = '/videos/game-launch.mp4';
 
-export const MobileAppVideo = () => {
-	const [open, setOpen] = useState(false);
-
-	return (
-		<>
-			<div className={styles.mobileAppVideo}>
-				<VideoButton
-					animate
-					onClick={() => setOpen(true)}
-					size={250}
-					src={APP_ICON}
-				/>
-			</div>
-
-			{open && <VideoModal onClose={() => setOpen(false)} src={VIDEO_SRC} />}
-		</>
-	);
-};
+export const MobileAppVideo = () => (
+	<div className={styles.mobileAppVideo}>
+		<video autoPlay controls loop muted playsInline src={VIDEO_SRC} />
+	</div>
+);
