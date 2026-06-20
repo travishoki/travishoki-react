41f0df32869a9093426cf095bada269e7f949726
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 02:55:48 -0600

    animate ai sparkle

--- src/pages/HomePage/AiPoweredProjects/AiPoweredProjects.tsx
+++ src/pages/HomePage/AiPoweredProjects/AiPoweredProjects.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import classNames from 'classnames';
+import { motion } from 'motion/react';
 import { Link } from 'react-router-dom';
 import { ProjectType } from 'src/pages/ProjectsPage/ProjectsPage.types';
 
 import { ImgPopIn } from '~animations/ImgPopIn';
 import { AitInfo } from '~components/AiInfo/AiInfo';
-import { SECONDARY_3 } from '~const/colors.const';
+import { GOLD_2, GOLD_4 } from '~const/colors.const';
 import ImgFoodCards from '~images/websites/thumbnail/food-cards.jpg';
 import { SvgIconSparkle } from '~svg/SvgIconSparkle';
 
@@ -16,6 +17,8 @@ import styles from './AiPoweredProjects.module.scss';
 
 const THUMBNAIL_SIZE = 250;
 
+const SPARKLE_COLORS = [GOLD_2, GOLD_4, GOLD_2, GOLD_4];
+
 export const AiPoweredProjects = () => {
 	const { aiIntegration } = AI_POWERED_FOOD as ProjectType;
 
@@ -23,7 +26,12 @@ export const AiPoweredProjects = () => {
 		<section className={styles.aiPoweredProjects}>
 			<div className="boxed boxed-lg">
 				<h2>
-					<SvgIconSparkle color={SECONDARY_3} size={28} />
+					<motion.span
+						animate={{ color: SPARKLE_COLORS }}
+						transition={{ duration: 3, ease: 'easeInOut', repeat: Infinity }}
+					>
+						<SvgIconSparkle color="currentColor" size={28} />
+					</motion.span>
 					AI Powered Project
 				</h2>
 				<div className={styles.container}>
