5c5878bce05463c6d647e935b27fa80840aafdfb
Travis Hoki <travis.hoki@gmail.com>
2026-06-20 15:11:17 -0600

    mike shinoda link

--- src/pages/HomePage/About/About.tsx
+++ src/pages/HomePage/About/About.tsx
@@ -5,6 +5,8 @@ import { Signature } from 'src/pages/HomePage/About/Signature/Signature';
 
 import { getYearsSince } from '~helpers/years';
 
+import { MikeShinoda } from './MikeShinoda/MikeShinoda';
+
 import styles from './About.module.scss';
 
 const MY_BIRTH_YEAR = '1990-07-08';
@@ -17,10 +19,10 @@ export const About = () => (
 			<p className="indent mb-0">
 				My name is Travis Hoki. I&apos;m from Pleasant Grove, UT. I&apos;m{' '}
 				{MY_AGE} years old. I drive a gray Honda Accord, its name is Bruce like
-				the shark in Finding Nemo. I&apos;ve been told I look like Mike Shinoda
-				from Linkin Park, or like a less attractive version of Keanu Reeves. I
-				look tired and unshaven but am nice. I&apos;m rocking a dad bod, but
-				people that don&apos;t like ice cream can’t be trusted.
+				the shark in Finding Nemo. I&apos;ve been told I look like{' '}
+				<MikeShinoda /> from Linkin Park, or like a less attractive version of
+				Keanu Reeves. I look tired and unshaven but am nice. I&apos;m rocking a
+				dad bod, but people that don&apos;t like ice cream can’t be trusted.
 			</p>
 			<Signature />
 		</div>
---
+++ src/pages/HomePage/About/MikeShinoda/MikeShinoda.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MikeShinoda = () => (
+	<a
+		href="https://en.wikipedia.org/wiki/Mike_Shinoda"
+		rel="noreferrer"
+		target="_blank"
+	>
+		Mike Shinoda
+	</a>
+);
