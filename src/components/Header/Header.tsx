b236dcc4d7fb1c4db2eb409d0f437b42d8dc520a
Travis Hoki <travis.hoki@gmail.com>
2026-06-22 12:19:15 -0600

    flex box

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import styles from './Header.module.scss';
 
 export const Header = () => (
 	<div className={styles.header}>
-		<div className="boxed-xl">
+		<div className={`boxed-xl ${styles.inner}`}>
 			<Title />
 			<Nav />
 		</div>
--- src/pages/ProjectsPage/Finder/Finder.tsx
+++ src/pages/ProjectsPage/Finder/Finder.tsx
@@ -55,8 +55,6 @@ export const Finder = ({
 				toggleOpenFilter={toggleOpenFilter}
 			/>
 
-			<div className="clearfix" />
-
 			<SearchForm
 				onChangeSearch={onChangeSearch}
 				onClearSearchTerm={onClearSearchTerm}
