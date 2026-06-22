490906c717db8675c98713b306bdd66c74603b39
Travis Hoki <travis.hoki@gmail.com>
2026-06-22 14:44:02 -0600

    ExternalLink and styling

--- src/pages/HowItsMadePage/HowItsMadeLinks/HowItsMadeLinks.tsx
+++ src/pages/HowItsMadePage/HowItsMadeLinks/HowItsMadeLinks.tsx
@@ -4,35 +4,30 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import styles from './HowItsMadeLinks.module.scss';
+import { ExternalLink } from '~components/ExternalLink/ExternalLink';
 
-const linkProps = {
-	rel: 'noreferrer',
-	target: '_blank',
-};
+import styles from './HowItsMadeLinks.module.scss';
 
 export const HowItsMadeLinks = () => {
 	return (
 		<section className="primary-three">
 			<div className={styles.howItsMadeLinks}>
-				<a
+				<ExternalLink
 					className={styles.howItsMadeLink}
 					href="https://github.com/travishoki/travishoki-react"
-					{...linkProps}
 				>
 					<FontAwesomeIcon icon={faGithub} />
 					<p className={styles.howItsMadeLinkLabel}>Github</p>
 					<p className={styles.howItsMadeLinkDescription}>Source Code</p>
-				</a>
-				<a
+				</ExternalLink>
+				<ExternalLink
 					className={styles.howItsMadeLink}
 					href="https://travishoki.com/storybook-static"
-					{...linkProps}
 				>
 					<FontAwesomeIcon icon={faBook} />
 					<p className={styles.howItsMadeLinkLabel}>Storybook</p>
 					<p className={styles.howItsMadeLinkDescription}>Component Library</p>
-				</a>
+				</ExternalLink>
 			</div>
 		</section>
 	);
